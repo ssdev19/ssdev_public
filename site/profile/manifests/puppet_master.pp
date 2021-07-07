e37073330559d7ef64ae706af669026dd6eb0085
AnonSS <mysticguy9@yahoo.com>
2021-07-07 15:10:41 -0700

    as

--- site/profile/manifests/puppet_master.pp
+++ site/profile/manifests/puppet_master.pp
@@ -1,6 +1,7 @@
 # puppet master config
 class profile::puppet_master {
   # The toml gem is required for grafana ldap.
+  # Be sure puppetserver service is restarted after the first run.
     package { 'toml':
       ensure   => present,
       provider => 'puppetserver_gem',
