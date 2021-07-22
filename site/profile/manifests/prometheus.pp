30691bf63f39dc8382b8aa6d5eea3d26f76c4579
AnonSS <mysticguy9@yahoo.com>
2021-07-21 17:01:19 -0700

    ss

--- site/profile/manifests/prometheus.pp
+++ site/profile/manifests/prometheus.pp
@@ -30,22 +30,21 @@ $advertise_ip,
   }
 # Alertmanager config
 class { 'prometheus::alertmanager':
-  version       => '0.22.2',
-  extra_options => "--cluster.advertise-address=${advertise_ip} \--cluster.listen-address=:9797 \--cluster.peer=${unwrap($cluster_hide)}",
-  route         => {
+  version   => '0.22.2',
+  route     => {
     'group_by'        => ['job'],
     'group_wait'      => '30s', # how long to wait to buffer alerts of the same group before sending a notification initially
     'group_interval'  => '45s',  # ^^before sending an alert that has been added to a group for which there has already been a notification
     'repeat_interval' => '3h',  # how long to wait before re-sending a given alert that has already been sent in a notification
     'receiver'        => 'slack',
   },
-  receivers     => [
+  receivers => [
       {
       'name'          => 'slack',
       'slack_configs' => [
         {
           'api_url'       => unwrap($slackapi_hide),
-          'channel'       => '#it_monitoring',
+          'channel'       => '#monitoring',
           'send_resolved' => true,
           'username'      => unwrap($slackuser_hide),
         },
