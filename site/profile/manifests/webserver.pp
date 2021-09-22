4baa73dc51c9521b45e79b9043f7fa964fbb0399
AnonSS <mysticguy9@yahoo.com>
2021-09-22 12:04:40 -0700

    include '::php'

--- site/profile/manifests/webserver.pp
+++ site/profile/manifests/webserver.pp
@@ -3,12 +3,13 @@ class profile::webserver {
 
 include mysql::server
 # PHP version
-class { '::php::globals':
-  php_version => '8.0.10',
-  config_root => '/etc/php/7.0',
-}
-class { '::php':
-  manage_repos => true
-}
+include '::php'
+# class { '::php::globals':
+#   php_version => '8.0.10',
+#   config_root => '/etc/php/7.0',
+# }
+# class { '::php':
+#   manage_repos => true
+# }
 }
 
