584fb93edf9e14f824af5ce3da446669b79aab63
AnonSS <mysticguy9@yahoo.com>
2021-09-27 16:21:57 -0700

    az

--- site/profile/manifests/webserver.pp
+++ site/profile/manifests/webserver.pp
@@ -2,12 +2,17 @@
 class profile::webserver {
 
 # include mysql::server
-class { 'mysql::server':
-  root_password           => 'strongpassword',
-  remove_default_accounts => true,
-  restart                 => true,
-  # override_options        => $override_options,
-}
+  class { 'mysql::server': 
+    root_password   => '123RTY*^',
+    remove_default_accounts => true,
+    restart => true,
+  } # end of class mysql::server
+  mysql::db { 'mydb':
+    user => 'jiradbuser',
+    password => 'DumbPss#2109874',
+    host => 'localhost',
+    grant => ['SELECT', 'UPDATE'],
+  } # end of mysql db
   # mysql::db { 'mydb':
   #   user     => 'mydbuser',
   #   password => 'changeme',
