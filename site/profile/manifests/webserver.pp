457342ccd3152dd1d10128bf67f8dd65ed7f6948
AnonSS <mysticguy9@yahoo.com>
2021-09-27 15:14:00 -0700

    aa

--- site/profile/manifests/webserver.pp
+++ site/profile/manifests/webserver.pp
@@ -1,13 +1,13 @@
 # webserver
 class profile::webserver {
 
-include mysql::server
-  # class { 'mysql::server':
-  # root_password           => 'changeme',
-  # remove_default_accounts => true,
-  # restart                 => true,
-  # # override_options        => $override_options,
-  # }
+# include mysql::server
+  class { 'mysql::server':
+  root_password           => 'changeme',
+  remove_default_accounts => true,
+  restart                 => true,
+  # override_options        => $override_options,
+  }
   mysql::db { 'mydb':
     user     => 'mydbuser',
     password => 'changeme',
