b1c0446d9f0461c3920bdfc370e7f9be324067af
AnonSS <mysticguy9@yahoo.com>
2021-09-27 14:52:34 -0700

    sx

--- site/profile/manifests/webserver.pp
+++ site/profile/manifests/webserver.pp
@@ -2,6 +2,12 @@
 class profile::webserver {
 
 include mysql::server
+  mysql::db { 'mydb':
+    user     => 'root',
+    password => 'mypass',
+    host     => 'localhost',
+    grant    => ['SELECT', 'UPDATE'],
+  }
 # PHP version
   class { 'apache':
   }
