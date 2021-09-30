a09bfed0b19e071478bb6c658de9030a17e38e5b
AnonSS <mysticguy9@yahoo.com>
2021-09-30 12:38:15 -0700

    za

--- site/profile/manifests/webserver.pp
+++ site/profile/manifests/webserver.pp
@@ -9,8 +9,12 @@ class profile::webserver {
   #   host     => 'localhost',
   #   grant    => ['SELECT', 'UPDATE'],
   # }
-# PHP version
-  class { 'apache':
+  apache::vhost { 'test1.us.lsst.org':
+    port     => '443',
+    docroot  => '/var/www/cert',
+    ssl      => true,
+    ssl_cert => '/etc/ssl/test1.us.lsst.org.cert',
+    ssl_key  => '/etc/ssl/test1.us.lsst.org.key',
   }
 # Below this line they only need to run once.  They can be commented out after first run.
   # Package { [ 'php-drush-drush', 'epel-release', 'yum-utils', 'http://rpms.remirepo.net/enterprise/remi-release-7.rpm' ]: #, 'http://rpms.remirepo.net/enterprise/remi-release-7.rpm'
@@ -20,6 +24,7 @@ class profile::webserver {
   #   command => 'yum-config-manager --enable remi-php74',
   #   path    => [ '/usr/local/bin/', '/bin/' ],  # alternative syntax
   # }
+# PHP version
 # include '::php'
   class { '::php::globals':
     php_version => '7.4.24',
