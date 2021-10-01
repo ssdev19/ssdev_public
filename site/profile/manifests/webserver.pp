ba9d936411dc8ef507d7886549d6ec0fa7f17290
AnonSS <mysticguy9@yahoo.com>
2021-10-01 10:16:29 -0700

    nginx

--- site/profile/manifests/webserver.pp
+++ site/profile/manifests/webserver.pp
@@ -9,13 +9,13 @@ class profile::webserver {
   #   host     => 'localhost',
   #   grant    => ['SELECT', 'UPDATE'],
   # }
-  class { 'apache':
-  default_vhost => false,
-  }
-  apache::vhost { 'test1.us.lsst.org':
-  port    => '80',
-  docroot => '/var/www/vhost',
-  }
+  # class { 'apache':
+  # default_vhost => false,
+  # }
+  # apache::vhost { 'test1.us.lsst.org':
+  # port    => '80',
+  # docroot => '/var/www/vhost',
+  # }
 # Below this line they only need to run once.  They can be commented out after first run.
   # Package { [ 'php-drush-drush', 'epel-release', 'yum-utils', 'http://rpms.remirepo.net/enterprise/remi-release-7.rpm' ]: #, 'http://rpms.remirepo.net/enterprise/remi-release-7.rpm'
   # ensure => installed,
@@ -26,8 +26,8 @@ class profile::webserver {
   # }
 # PHP version
 # include '::php'
-  class { '::php::globals':
-    php_version => '7.4.24',
-    config_root => '/etc/php/7.0',
-  }
+  # class { '::php::globals':
+  #   php_version => '7.4.24',
+  #   config_root => '/etc/php/7.0',
+  # }
 }
