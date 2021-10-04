1b0af21da45d013f0d2e5a0d245a6656e117e810
AnonSS <mysticguy9@yahoo.com>
2021-10-04 14:27:43 -0700

    aa

--- site/profile/manifests/webserver.pp
+++ site/profile/manifests/webserver.pp
@@ -17,13 +17,13 @@ include nginx
   # docroot => '/var/www/vhost',
   # }
 # Below this line they only need to run once.  They can be commented out after first run.
-  # Package { [ 'php-drush-drush', 'epel-release', 'yum-utils', 'http://rpms.remirepo.net/enterprise/remi-release-7.rpm' ]: #, 'http://rpms.remirepo.net/enterprise/remi-release-7.rpm'
-  # ensure => installed,
-  # }
-  # exec { 'yum-config-manager':
-  #   command => 'yum-config-manager --enable remi-php74',
-  #   path    => [ '/usr/local/bin/', '/bin/' ],  # alternative syntax
-  # }
+  Package { [ 'php-drush-drush', 'epel-release', 'yum-utils', 'http://rpms.remirepo.net/enterprise/remi-release-7.rpm' ]: #, 'http://rpms.remirepo.net/enterprise/remi-release-7.rpm'
+  ensure => installed,
+  }
+  exec { 'yum-config-manager':
+    command => 'yum-config-manager --enable remi-php74',
+    path    => [ '/usr/local/bin/', '/bin/' ],  # alternative syntax
+  }
 # PHP version
 # include '::php'
   class { '::php::globals':
