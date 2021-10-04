3fc967d75386b24652a8ef9b9b4186400d27a0d0
AnonSS <mysticguy9@yahoo.com>
2021-10-04 14:00:37 -0700

    xx

--- site/profile/manifests/webserver.pp
+++ site/profile/manifests/webserver.pp
@@ -30,6 +30,9 @@ include nginx
     php_version => '7.4.24',
     config_root => '/etc/php/7.0',
   }
+  class { '::php':
+    manage_repos => true
+  }
 #   /etc/nginx/YOURLS/user/config.php #contains config settings for the YOURLS app to connect to its mysql server, time settings, and the webserver. It also stores local users authorized to login to the yourls admin page.
 # /etc/nginx/conf.d/yourls.conf #nginx conf file for YOURLS website and webpages.
 # /etc/php-fpm.d/*.conf #php-fpm must be configured properly and running for YOURLS to render properly.
