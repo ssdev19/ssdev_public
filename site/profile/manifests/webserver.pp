54884a2d0e06837b0296c927a09e8ca7c9aad64b
AnonSS <mysticguy9@yahoo.com>
2021-10-09 15:00:25 -0700

    zaq

--- site/profile/manifests/webserver.pp
+++ site/profile/manifests/webserver.pp
@@ -15,13 +15,13 @@ include nginx
 # include mysql::server
 
 # Below this line they only need to run once.  They can be commented out after first run.
-  # Package { [ 'php-drush-drush', 'epel-release', 'yum-utils', 'http://rpms.remirepo.net/enterprise/remi-release-7.rpm' ]:
-  # ensure => installed,
-  # }
-  # exec { 'yum-config-manager':
-  #   command => 'yum-config-manager --enable remi-php73',
-  #   path    => [ '/usr/local/bin/', '/bin/' ],  # alternative syntax
-  # }
+  Package { [ 'php-drush-drush', 'epel-release', 'yum-utils', 'http://rpms.remirepo.net/enterprise/remi-release-7.rpm' ]:
+  ensure => installed,
+  }
+  exec { 'yum-config-manager':
+    command => 'yum-config-manager --enable remi-php73',
+    path    => [ '/usr/local/bin/', '/bin/' ],  # alternative syntax
+  }
 # PHP version
 # include '::php'
   class { '::php::globals':
@@ -30,8 +30,8 @@ include nginx
   }
   -> class { '::php':
     manage_repos => false,
-    # fpm_user     => 'nginx',
-    # fpm_group    => 'nginx',
+    fpm_user     => 'nginx',
+    fpm_group    => 'nginx',
   }
 #   /etc/nginx/YOURLS/user/config.php #contains config settings for the YOURLS app to connect to its mysql server, time settings, 
 # and the webserver. It also stores local users authorized to login to the yourls admin page.
