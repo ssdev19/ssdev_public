1b179ab8d53a4f4cf051d76f0b45dee60230c001
AnonSS <mysticguy9@yahoo.com>
2021-11-29 08:29:14 -0700

    dc

--- site/profile/manifests/puppet_master2.pp
+++ site/profile/manifests/puppet_master2.pp
@@ -3,8 +3,8 @@ class profile::puppet_master2 {
   include r10k
   # The toml gem is required for grafana ldap.
   # Be sure puppetserver service is restarted after the first run.
-    package { 'toml':
-      ensure   => present,
-      provider => 'puppetserver_gem',
-    }
+    # package { 'toml':
+    #   ensure   => present,
+    #   provider => 'puppetserver_gem',
+    # }
   }
