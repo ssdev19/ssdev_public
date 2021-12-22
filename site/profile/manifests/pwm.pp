37270683f35ebde8f6777c22721dc19683c3fc58
AnonSS <mysticguy9@yahoo.com>
2021-12-22 13:24:24 -0700

    sd

---
+++ site/profile/manifests/pwm.pp
@@ -0,0 +1,13 @@
+## Reboot will be required following the installation of this
+class profile::pwm {
+  # include firewalld
+  archive { '/tmp/pwm.war':
+    ensure   => present,
+    source   => 'https://github.com/pwm-project/pwm/releases/download/v1_9_2/pwm-1.9.2.war',
+    provider => 'wget',
+    cleanup  => false,
+  }
+  file { '/tmp/pwm.war':
+    ensure => present,
+    source => '/opt/tomcat/webapps/pwm.war',
+  }
