dd5b7f3239de182b698f4161feafd02ab2459423
AnonSS <mysticguy9@yahoo.com>
2021-12-22 13:19:04 -0700

    as

--- site/profile/manifests/tomcat.pp
+++ site/profile/manifests/tomcat.pp
@@ -49,7 +49,7 @@ $distribution,
     ensure => present,
   }
   -> file_line{ 'remove_old_exports_aliases':
-      match => ['<Valve className="org.apache.catalina.valves.RemoteAddrValve"', 'allow="127\.\d+\.\d+\.\d+|::1|0:0:0:0:0:0:0:1" />'],
+      match => ['<Valve className="org.apache.catalina.valves.RemoteAddrValve"'],
       line  => ' ',
       path  => '/opt/tomcat/webapps/manager/META-INF/context.xml',
     }
