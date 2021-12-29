6476e6c15282e2dd59a117ce3fad710393850d23
AnonSS <mysticguy9@yahoo.com>
2021-12-29 11:59:13 -0700

    sd

--- site/profile/manifests/pwm.pp
+++ site/profile/manifests/pwm.pp
@@ -1,32 +1,5 @@
 ## Reboot will be required following the installation of this
 class profile::pwm {
-
-  archive { '/tmp/pwm-1.9.2.war':
-    ensure   => present,
-    source   => 'https://github.com/pwm-project/pwm/releases/download/v1_9_2/pwm-1.9.2.war',
-    provider => 'wget',
-    cleanup  => false,
-  }
-  file { '/opt/tomcat/webapps/pwm.war':
-    ensure => present,
-    source => '/tmp/pwm-1.9.2.war',
-  }
-  $applicationpath = lookup('application_path')
-  $webpath = lookup('web_path')
-  file { '/opt/tomcat/webapps/pwm/WEB-INF/web.xml':
-    ensure => present,
-  }
-  -> file_line { 'Append a line to pwm/WEB-INF/web.xml':
-      path  => $webpath,
-      line  => "<param-value>${applicationpath}</param-value>",
-      match => '<param-value>unspecified</param-value>', # "^unspecified.*$" can be used for string
-    }
-  # pwm configuration
-  # $pwmconfig = lookup('pwmconfig')
-  # file { '/opt/tomcat/webapps/pwm/WEB-INF/PwmConfiguration.xml':
-  #   ensure  => present,
-  #   content => $pwmconfig,
-  # }
   $pwmconfig_dest = lookup('pwmconfig_dest')
   $pwmconfig_source = lookup('pwmconfig_source')
   archive { $pwmconfig_dest :
@@ -40,6 +13,33 @@ class profile::pwm {
     source  => $pwmkeystore,
     cleanup => false,
   }
+  archive { '/tmp/pwm-1.9.2.war':
+    ensure   => present,
+    source   => 'https://github.com/pwm-project/pwm/releases/download/v1_9_2/pwm-1.9.2.war',
+    provider => 'wget',
+    cleanup  => false,
+  }
+  file { '/opt/tomcat/webapps/pwm.war':
+    ensure => present,
+    source => '/tmp/pwm-1.9.2.war',
+  }
+  # $applicationpath = lookup('application_path')
+  # $webpath = lookup('web_path')
+  # file { '/opt/tomcat/webapps/pwm/WEB-INF/web.xml':
+  #   ensure => present,
+  # }
+  # -> file_line { 'Append a line to pwm/WEB-INF/web.xml':
+  #     path  => $webpath,
+  #     line  => "<param-value>${applicationpath}</param-value>",
+  #     match => '<param-value>unspecified</param-value>', # "^unspecified.*$" can be used for string
+  #   }
+  # pwm configuration
+  # $pwmconfig = lookup('pwmconfig')
+  # file { '/opt/tomcat/webapps/pwm/WEB-INF/PwmConfiguration.xml':
+  #   ensure  => present,
+  #   content => $pwmconfig,
+  # }
+
   # # Manage certs
   # java_ks { 'pwm:truststore':
   #   ensure       => latest,
