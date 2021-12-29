1c68234a78fdea7c51f6aa453c2a97579f71295a
AnonSS <mysticguy9@yahoo.com>
2021-12-29 12:09:47 -0700

    ss

--- site/profile/manifests/pwm.pp
+++ site/profile/manifests/pwm.pp
@@ -1,18 +1,12 @@
 ## Reboot will be required following the installation of this
 class profile::pwm {
+  exec { 'wait_for_tomcat_service' :
+  require => Service['tomcat'],
+  command => '/usr/bin/wget --spider --tries 10 --retry-connrefused --no-check-certificate http://localhost:8080',
+  path    => '/usr/bin:/bin',
+}
   $pwmconfig_dest = lookup('pwmconfig_dest')
   $pwmconfig_source = lookup('pwmconfig_source')
-  archive { $pwmconfig_dest :
-    ensure  => present,
-    source  => $pwmconfig_source,
-    cleanup => false,
-  }
-  $pwmkeystore = lookup('pwmkeystore')
-  archive { '/etc/pki/keystore' :
-    ensure  => present,
-    source  => $pwmkeystore,
-    cleanup => false,
-  }
   archive { '/tmp/pwm-1.9.2.war':
     ensure   => present,
     source   => 'https://github.com/pwm-project/pwm/releases/download/v1_9_2/pwm-1.9.2.war',
@@ -23,7 +17,18 @@ class profile::pwm {
     ensure => present,
     source => '/tmp/pwm-1.9.2.war',
   }
-  # $applicationpath = lookup('application_path')
+    archive { $pwmconfig_dest :
+    ensure  => present,
+    source  => $pwmconfig_source,
+    cleanup => false,
+  }
+  $pwmkeystore = lookup('pwmkeystore')
+  archive { '/etc/pki/keystore' :
+    ensure  => present,
+    source  => $pwmkeystore,
+    cleanup => false,
+  }
+# $applicationpath = lookup('application_path')
   # $webpath = lookup('web_path')
   # file { '/opt/tomcat/webapps/pwm/WEB-INF/web.xml':
   #   ensure => present,
