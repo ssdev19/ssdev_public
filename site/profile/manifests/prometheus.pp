2b12205cd80616bc9b1056bbe324b12af7453b00
AnonSS <mysticguy9@yahoo.com>
2022-01-07 14:01:01 -0700

    sd

--- site/profile/manifests/prometheus.pp
+++ site/profile/manifests/prometheus.pp
@@ -29,13 +29,13 @@ $slackuser_hide,
 # Alertmanager config
 class { 'prometheus::alertmanager':
   version   => '0.22.2',
-  route     => {
-    'group_by'        => ['job'],
-    'group_wait'      => '30s', # how long to wait to buffer alerts of the same group before sending a notification initially
-    'group_interval'  => '45s',  # ^^before sending an alert that has been added to a group for which there has already been a notification
-    'repeat_interval' => '3h',  # how long to wait before re-sending a given alert that has already been sent in a notification
-    'receiver'        => 'slack',
-  },
+  # route     => {
+  #   'group_by'        => ['job'],
+  #   'group_wait'      => '30s', # how long to wait to buffer alerts of the same group before sending a notification initially
+  #   'group_interval'  => '45s',  # ^^before sending an alert that has been added to a group for which there has already been a notification
+  #   'repeat_interval' => '3h',  # how long to wait before re-sending a given alert that has already been sent in a notification
+  #   'receiver'        => 'slack',
+  # },
   receivers => [
       {
       'name'          => 'slack',
