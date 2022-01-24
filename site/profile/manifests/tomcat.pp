baa5e138e32036bdb03e1350b9cc4c9b58f371a7
AnonSS <mysticguy9@yahoo.com>
2022-01-24 16:01:21 -0700

    sd

--- site/profile/manifests/tomcat.pp
+++ site/profile/manifests/tomcat.pp
@@ -10,41 +10,6 @@ $https_enabled,
 $keystorepass_hide,
 $ciphers,
 ){
-# Getting tomcat::service to work was too painful
-  $tomcat_service = @("EOT")
-    [Unit]
-    Description=Apache Tomcat Web Application Container
-    After=syslog.target network.target
-
-    [Service]
-    Type=forking
-    SuccessExitStatus=143
-
-    Environment=JAVA_HOME=${java_home}
-    Environment=CATALINA_PID=${catalina_home}/temp/tomcat.pid
-    Environment=CATALINA_HOME=${catalina_home}
-    Environment=CATALINA_BASE=${catalina_base}
-    Environment='CATALINA_OPTS=-Xms512M -Xmx1024M -server -XX:+UseParallelGC'
-    Environment='JAVA_OPTS=-Djava.awt.headless=true -Djava.security.egd=file:/dev/./urandom'
-
-    ExecStart=${catalina_home}/bin/startup.sh
-    ExecStop=${catalina_home}/bin/shutdown.sh
-
-    User=tomcat
-    Group=tomcat
-
-    [Install]
-    WantedBy=multi-user.target
-    | EOT
-# 
-  systemd::unit_file { 'tomcat.service':
-    content => "${tomcat_service}",
-  }
-  -> service { 'tomcat':
-  subscribe => Tomcat::Instance['default'],
-  ensure    => 'running',
-  enable    => true,
-  }
 
   tomcat::install { $catalina_home:
   source_url     => "https://dlcdn.apache.org/tomcat/${version}.tar.gz",
@@ -100,5 +65,39 @@ $ciphers,
       'keystoreFile'        => '/etc/pki/keystore',
     },
   }
+# Getting tomcat::service to work was too painful
+  $tomcat_service = @("EOT")
+    [Unit]
+    Description=Apache Tomcat Web Application Container
+    After=syslog.target network.target
 
+    [Service]
+    Type=forking
+    SuccessExitStatus=143
+
+    Environment=JAVA_HOME=${java_home}
+    Environment=CATALINA_PID=${catalina_home}/temp/tomcat.pid
+    Environment=CATALINA_HOME=${catalina_home}
+    Environment=CATALINA_BASE=${catalina_base}
+    Environment='CATALINA_OPTS=-Xms512M -Xmx1024M -server -XX:+UseParallelGC'
+    Environment='JAVA_OPTS=-Djava.awt.headless=true -Djava.security.egd=file:/dev/./urandom'
+
+    ExecStart=${catalina_home}/bin/startup.sh
+    ExecStop=${catalina_home}/bin/shutdown.sh
+
+    User=tomcat
+    Group=tomcat
+
+    [Install]
+    WantedBy=multi-user.target
+    | EOT
+# 
+  systemd::unit_file { 'tomcat.service':
+    content => "${tomcat_service}",
+  }
+  -> service { 'tomcat':
+  subscribe => Tomcat::Instance['default'],
+  ensure    => 'running',
+  enable    => true,
+  }
 }
