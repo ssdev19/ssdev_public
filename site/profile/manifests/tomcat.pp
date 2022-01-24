ad92bd040bba1f2953f90556ef6840d3716eb002
AnonSS <mysticguy9@yahoo.com>
2022-01-24 11:54:16 -0700

    sd

--- site/profile/manifests/tomcat.pp
+++ site/profile/manifests/tomcat.pp
@@ -31,14 +31,14 @@ $ciphers,
   # ensure        => 'absent',
   # catalina_base => $catalina_base,
   # }
-  file { '/opt/tomcat/webapps/manager/META-INF/context.xml':
-    ensure => file,
-  }
-  -> file_line{ 'remove org.apache.catalina.valves.RemoteAddrValve':
-      match => 'org.apache.catalina.valves.RemoteAddrValve',
-      line  => ' ',
-      path  => '/opt/tomcat/webapps/manager/META-INF/context.xml',
-    }
+  # file { '/opt/tomcat/webapps/manager/META-INF/context.xml':
+  #   ensure => file,
+  # }
+  # -> file_line{ 'remove org.apache.catalina.valves.RemoteAddrValve':
+  #     match => 'org.apache.catalina.valves.RemoteAddrValve',
+  #     line  => ' ',
+  #     path  => '/opt/tomcat/webapps/manager/META-INF/context.xml',
+  #   }
   tomcat::config::server::tomcat_users { unwrap($tomcat_user_hide):
     password      => $tomcat_pass_hide.unwrap,
     roles         => ['admin-gui, manager-gui, manager-script'],
