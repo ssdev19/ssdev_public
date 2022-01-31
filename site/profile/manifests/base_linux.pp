3ed1c81ef423a576eabba354c07fb7d79a8fadaf
AnonSS <mysticguy9@yahoo.com>
2022-01-31 13:07:05 -0700

    ss

--- site/profile/manifests/base_linux.pp
+++ site/profile/manifests/base_linux.pp
@@ -67,21 +67,24 @@ class profile::base_linux (
 # install awscli tool
 # class { 'awscli': }
 if $awscli {
+    Package { [ 'awscli' ]:
+      ensure => installed,
+    }
   # archive { '/tmp/awscli-bundle.zip':
   #   ensure   => present,
   #   source   => 'https://s3.amazonaws.com/aws-cli/awscli-bundle.zip',
   #   provider => 'wget',
   #   cleanup  => false,
   # }
-  archive { '/tmp/awscli-bundle.zip':
-    path         => '/tmp',
-    source       => 'https://s3.amazonaws.com/aws-cli/awscli-bundle.zip',
-    extract      => true,
-    extract_path => '/usr/local/aws',
-    creates      => '/usr/local/bin',
-    cleanup      => true,
-  # require      => File['wso2_appdir'],
-  }
+  # archive { '/tmp/awscli-bundle.zip':
+  #   path         => '/tmp',
+  #   source       => 'https://s3.amazonaws.com/aws-cli/awscli-bundle.zip',
+  #   extract      => true,
+  #   extract_path => '/usr/local/aws',
+  #   creates      => '/usr/local/bin',
+  #   cleanup      => true,
+  # # require      => File['wso2_appdir'],
+  # }
   # file { '/opt/tomcat/webapps/pwm.war':
   #   ensure => present,
   #   source => '/tmp/pwm-1.9.2.war',
