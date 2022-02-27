5741fe90a67e737493a5e57dd5355a5b59742338
AnonSS <mysticguy9@yahoo.com>
2022-02-27 12:41:02 -0700

    sx

--- site/profile/manifests/tomcat.pp
+++ site/profile/manifests/tomcat.pp
@@ -51,7 +51,7 @@ $ciphers,
     protocol              =>'org.apache.coyote.http11.Http11NioProtocol', # $http_version,
     purge_connectors      => true,
     additional_attributes => {
-      'redirectPort'        => absent,
+      'redirectPort'        => '80',
       'SSLEnabled'          => $https_enabled,
       'maxThreads'          => 150,
       'scheme'              => https,
