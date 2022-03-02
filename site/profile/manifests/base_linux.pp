cf0c78eed6d32e219867889fb3fcd6cd12641706
AnonSS <mysticguy9@yahoo.com>
2022-03-02 10:18:22 -0700

    as

--- site/profile/manifests/base_linux.pp
+++ site/profile/manifests/base_linux.pp
@@ -61,7 +61,7 @@ class profile::base_linux (
   }
   Package { [ 'git', 'tree', 'tcpdump', 'telnet', 'lvm2', 'gcc', 'xinetd',
   'bash-completion', 'sudo', 'vim', 'openssl', 'openssl-devel',
-  'acpid', 'wget', 'nmap', 'iputils', 'bind-utils', 'traceroute' ]:
+  'acpid', 'wget', 'nmap', 'iputils', 'bind-utils', 'traceroute', 'unzip' ]:
   ensure => installed,
   }
 # install awscli tool
