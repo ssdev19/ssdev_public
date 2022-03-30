6445f78555859d4fbc56f5036c211115215822c8
anonss <mysticguy9@yahoo.com>
2022-03-30 12:47:21 -0700

    ss

--- site/profile/manifests/pwm.pp
+++ site/profile/manifests/pwm.pp
@@ -100,14 +100,14 @@ include 'archive'
 
 
   # # Manage certs
-  java_ks { 'dc2.lsst.local:/usr/java/jdk-11.0.2+9-jre/lib/security/cacerts':
+  java_ks { 'dc2.lsst.local:/usr/java/jdk-11.0.2+9-jre/lib/security/cacerts2':
     ensure       => latest,
     certificate  => '/tmp/DC2Cert.cer',
     # target       => '/usr/java/jdk-11.0.2+9-jre/lib/security/cacerts',
     password     => $keystorepwd, # Must be at least 6 characters
     trustcacerts => true,
   }
-  java_ks { 'dc3.lsst.local:/usr/java/jdk-11.0.2+9-jre/lib/security/cacerts':
+  java_ks { 'dc3.lsst.local:/usr/java/jdk-11.0.2+9-jre/lib/security/cacerts2':
     ensure       => latest,
     certificate  => '/tmp/DC3Cert.cer',
     # target       => '/usr/java/jdk-11.0.2+9-jre/lib/security/cacerts',
