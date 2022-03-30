6ae902dc67df9f7630de6583920fdc1f6eae98fc
anonss <mysticguy9@yahoo.com>
2022-03-30 16:45:42 -0700

    dd

--- site/profile/manifests/java.pp
+++ site/profile/manifests/java.pp
@@ -24,7 +24,7 @@ $mem,
   #   java          => 'jdk',
   # }
   java::adopt { $distribution :
-    ensure  => present,
+    ensure  => installed,
     version => $j_version,
     java    => $distribution,
     # require => File['/usr/java/jdk-11.0.2+9-jre/lib/security/cacerts']
