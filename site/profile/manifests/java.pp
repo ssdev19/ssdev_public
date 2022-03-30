af733102771d16e33e85f6a1f7802f20596b5f42
anonss <mysticguy9@yahoo.com>
2022-03-30 16:07:35 -0700

    as

--- site/profile/manifests/java.pp
+++ site/profile/manifests/java.pp
@@ -23,11 +23,12 @@ $mem,
   #   version_minor => $version_minor,
   #   java          => 'jdk',
   # }
-  # java::adopt { $distribution :
-  #   ensure  => 'present',
-  #   version => $j_version,
-  #   java    => $distribution,
-  # }
+  java::adopt { $distribution :
+    ensure  => 'present',
+    version => $j_version,
+    java    => $distribution,
+    unless  => '/usr/java/jdk-11.0.2+9-jre/lib/security/cacerts'
+  }
   ### export _JAVA_OPTIONS="-Xmx1g"
   exec { 'set java heap size ':
     path    => [ '/usr/bin', '/bin', '/usr/sbin' ],
