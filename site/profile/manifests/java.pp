6704165b146caeca6fa532806623fcbd41b644c3
anonss <mysticguy9@yahoo.com>
2022-03-31 11:35:12 -0700

    dd

--- site/profile/manifests/java.pp
+++ site/profile/manifests/java.pp
@@ -23,12 +23,12 @@ $mem,
   #   version_minor => $version_minor,
   #   java          => 'jdk',
   # }
-    # java::adopt { $distribution :
-    #   ensure  => 'present',
-    #   version => $j_version,
-    #   java    => $distribution,
-    #   # basedir => '/usr/java/',
-    # }
+    java::adopt { 'AdoptOpenJDK java jre 11 11.0.2 9' :
+      ensure  => 'present',
+      version => $j_version,
+      java    => $distribution,
+      # basedir => '/usr/java/',
+    }
   ### export _JAVA_OPTIONS="-Xmx1g"
   exec { 'set java heap size ':
     path    => [ '/usr/bin', '/bin', '/usr/sbin' ],
