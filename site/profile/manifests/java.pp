b78911d8737ff3eab6f3799cc5faaf01a0291214
anonss <mysticguy9@yahoo.com>
2022-03-31 13:40:42 -0700

    as

--- site/profile/manifests/java.pp
+++ site/profile/manifests/java.pp
@@ -34,8 +34,8 @@ $mem,
     path    => [ '/usr/bin', '/bin', '/usr/sbin' ],
     command => "sudo -s export _JAVA_OPTIONS=${mem}",
   }
-  exec { 'set java path':
-    path    => [ '/usr/bin', '/bin', '/usr/sbin' ],
-    command => "sudo -s export PATH=${java_path}",
-  }
+  # exec { 'set java path':
+  #   path    => [ '/usr/bin', '/bin', '/usr/sbin' ],
+  #   command => "sudo -s export PATH=${java_path}",
+  # }
 }
