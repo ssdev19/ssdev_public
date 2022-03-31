f8b5e532f6db30e4622f42c5b54bc10609243ee9
anonss <mysticguy9@yahoo.com>
2022-03-31 07:58:20 -0700

    sd

--- site/profile/manifests/java.pp
+++ site/profile/manifests/java.pp
@@ -11,6 +11,25 @@ $java_home,
 $java_path,
 $mem,
 ){
+  class { 'java':
+    distribution => $distribution,
+    version      => $j_version,
+    # java_home    => $java_home,
+  }
+  # java::adopt { 'jdk' :
+  #   ensure        => 'present',
+  #   version       => $jdk_version,
+  #   version_major => $version_major,
+  #   version_minor => $version_minor,
+  #   java          => 'jdk',
+  # }
+    # java::adopt { $distribution :
+    #   ensure  => 'present',
+    #   version => $j_version,
+    #   java    => $distribution,
+    #   # require => File['/usr/java/jdk-11.0.2+9-jre/lib/security/cacerts']
+    # }
+  ### export _JAVA_OPTIONS="-Xmx1g"
   exec { 'set java heap size ':
     path    => [ '/usr/bin', '/bin', '/usr/sbin' ],
     command => "sudo -s export _JAVA_OPTIONS=${mem}",
@@ -19,23 +38,4 @@ $mem,
     path    => [ '/usr/bin', '/bin', '/usr/sbin' ],
     command => "sudo -s export PATH=${java_path}",
   }
-  # class { 'java':
-  #   distribution => $distribution,
-  #   version      => $version,
-  #   java_home    => $java_home,
-  # }
-  # java::adopt { 'jdk' :
-  #   ensure        => 'present',
-  #   version       => $jdk_version,
-  #   version_major => $version_major,
-  #   version_minor => $version_minor,
-  #   java          => 'jdk',
-  # }
-    java::adopt { $distribution :
-      ensure  => 'present',
-      version => $j_version,
-      java    => $distribution,
-      # require => File['/usr/java/jdk-11.0.2+9-jre/lib/security/cacerts']
-    }
-  ### export _JAVA_OPTIONS="-Xmx1g"
 }
