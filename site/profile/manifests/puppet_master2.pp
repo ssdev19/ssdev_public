8a1b49a26d1c095ea0d3d658b24b792388448a4d
anonss <mysticguy9@yahoo.com>
2022-04-11 11:27:34 -0700

    sd

--- site/profile/manifests/puppet_master2.pp
+++ site/profile/manifests/puppet_master2.pp
@@ -4,11 +4,10 @@ class profile::puppet_master2 {
   include foreman
   include foreman::cli
   include foreman::compute::libvirt
-  include foreman::compute::vmware
   include foreman::plugin::remote_execution
   include foreman::plugin::tasks
   # include foreman_proxy
-  include foreman_proxy::plugin::dns::route53
+  # include foreman_proxy::plugin::dns::route53
   include foreman_proxy::plugin::dynflow
   include foreman_proxy::plugin::remote_execution::ssh
   include foreman::repo
