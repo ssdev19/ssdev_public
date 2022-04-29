faae99219ec76406854eef469cc7d0a69d5b07ef
anonss <mysticguy9@yahoo.com>
2022-04-29 15:30:39 -0700

    df

--- site/profile/manifests/puppet_master.pp
+++ site/profile/manifests/puppet_master.pp
@@ -10,7 +10,7 @@ class profile::puppet_master {
   archive { '/tmp/pingfederate11':
     ensure   => present,
     source   => 'https://auraastronomy-my.sharepoint.com/:f:/g/personal/rrichmond_aura-astronomy_org/Ety_d7IdJDdIgD1X4y5IrMABQ22ezhyRiPerGFOK8dwFGQ?e=kPtsWH',
-    provider => 'wget',
+    # provider => 'wget',
     cleanup  => false,
   }
 }
