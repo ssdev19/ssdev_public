21f7756bea08f2551e180b639aeda3fda0ef4294
anonss <mysticguy9@yahoo.com>
2022-05-04 13:49:49 -0700

    ss

--- site/profile/manifests/sso.pp
+++ site/profile/manifests/sso.pp
@@ -1,5 +1,11 @@
 #  PingFed config
-class profile::sso {
+class profile::sso ( String
+$pf_user,
+$pf_pass,
+$java_home,
+$pf_home,
+$pf_version,
+){
 include 'archive'
     file {
       '/opt/pingfederate':
@@ -17,5 +23,30 @@ include 'archive'
   command  => '/opt/pingfederate/pingfederate-11.0.2/pingfederate/bin/run.sh',
   provider => shell,
   # onlyif   => '/usr/bin/test -e /path/to/file/test.txt',
+  }
+  $pingfederate_service = @("EOT")
+    [Unit]
+    Description=PingFederate ${pf_version}
+    Documentation=https://support.pingidentity.com/s/PingFederate-help
+
+    [Install]
+    WantedBy=multi-user.target
+
+    [Service]
+    Type=simple
+    User=${pf_user}
+    WorkingDirectory=${pf_home}
+    Environment='JAVA_HOME=${java_home}'
+    ExecStart=${pf_home}/bin/run.sh
+
+    | EOT
+# 
+  systemd::unit_file { 'pingfederate.service':
+    content => "${pingfederate_service}",
+  }
+  -> service { 'pingfederate':
+  # subscribe => Pingfederate::Instance['default'],
+  ensure    => 'running',
+  enable    => true,
   }
 }
