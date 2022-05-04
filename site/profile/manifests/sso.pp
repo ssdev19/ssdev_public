6041f565e693e45b5fff3c58e7eaeb46561ee799
anonss <mysticguy9@yahoo.com>
2022-05-04 11:38:26 -0700

    ss

--- site/profile/manifests/sso.pp
+++ site/profile/manifests/sso.pp
@@ -8,7 +8,7 @@ include 'archive'
   archive { '/tmp/pingfed.zip':
     # ensure   => present,
     source   => 'https://project.lsst.org/zpuppet/pingfederate/pingfederate-11.0.2.zip',
-    provider => 'wget',
+    # provider => 'wget',
     cleanup  => false,
     extract      => true,
     extract_path => '/tmp/pf_install',
