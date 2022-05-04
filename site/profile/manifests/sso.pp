14a437f2afd9a772a05c64626b0ea06d533547d1
anonss <mysticguy9@yahoo.com>
2022-05-04 14:23:22 -0700

    sd

--- site/profile/manifests/sso.pp
+++ site/profile/manifests/sso.pp
@@ -11,23 +11,23 @@ $pf_version,
     password => $pf_pass
   }
 include 'archive'
-    file {
-      '/opt/pingfederate':
-        ensure => directory,
-        owner  => $pf_user,
-        group  => $pf_user,
-        mode   => '0775',
-    }
+    # file {
+    #   '/opt/pingfederate':
+    #     ensure => directory,
+    #     owner  => $pf_user,
+    #     group  => $pf_user,
+    #     mode   => '0775',
+    # }
   archive { '/tmp/pingfed.zip':
     # ensure   => present,
     source       => 'https://project.lsst.org/zpuppet/pingfederate/pingfederate-11.0.2.zip',
     # provider => 'wget',
     cleanup      => false,
     extract      => true,
-    extract_path => '/opt/pingfederate',
+    extract_path => '/opt',
   }
   exec {'Install pingfed':
-  command  => '/opt/pingfederate/pingfederate-11.0.2/pingfederate/bin/run.sh',
+  command  => '/opt/pingfederate-11.0.2/pingfederate/bin/run.sh',
   provider => shell,
   # onlyif   => '/usr/bin/test -e /path/to/file/test.txt',
   }
