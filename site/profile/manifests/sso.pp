3d8d2d53fb8b3217ec8cac2d08bfe91004c0ab68
anonss <mysticguy9@yahoo.com>
2022-05-04 11:32:48 -0700

    aa

--- site/profile/manifests/sso.pp
+++ site/profile/manifests/sso.pp
@@ -1,6 +1,10 @@
 #  PingFed config
 class profile::sso {
 include 'archive'
+    file {
+      '/tmp/pf_install':
+        ensure => directory,
+    }
   archive { '/tmp/pingfed.zip':
     # ensure   => present,
     source   => 'https://project.lsst.org/zpuppet/pingfederate/pingfederate-11.0.2.zip',
