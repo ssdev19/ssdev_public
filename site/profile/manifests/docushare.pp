c9e20ce49a25e024482d7bc741a0b97492a56060
anonss <mysticguy9@yahoo.com>
2022-06-24 13:23:06 -0700

    dd

--- site/profile/manifests/docushare.pp
+++ site/profile/manifests/docushare.pp
@@ -1,15 +1,19 @@
-# https://download.support.xerox.com/pub/docs/docushare/userdocs/any-os/en_GB/install_gde652.pdf
+# https://download.support.xerox.com/pub/docs/docushare/userdocs/any-os/en_GB/install_gde652.pdf 
 class profile::docushare {
 include 'archive'
 
 #download docushare
+  file {
+    '/opt/xerox':
+      ensure => directory,
+  }
   archive { '/tmp/docushare.tar.gz':
     # ensure   => present,
     source       => 'https://download.support.xerox.com/pub/drivers/docushare/utils/wins2016x64/en_GB/ds750-b215-linux.tar.gz',
     # provider => 'wget',
     cleanup      => true,
     extract      => true,
-    extract_path => '/opt/',
+    extract_path => '/opt/xerox',
   }
 
 }
