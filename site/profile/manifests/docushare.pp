86836697f97f60b43ab5ebe4bc8819d1124b5ccd
anonss <mysticguy9@yahoo.com>
2022-06-24 14:19:24 -0700

    aa

--- site/profile/manifests/docushare.pp
+++ site/profile/manifests/docushare.pp
@@ -16,7 +16,7 @@ include 'archive'
   $path = '$PATH:/opt/xerox/Linux/DocuShare'
   exec { 'set DocuShare path':
     path    => [ '/usr/bin', '/bin', '/usr/sbin' ],
-    command => "PATH=${path}",
+    command => "sudo -s PATH=${path}",
   }
 
 }
