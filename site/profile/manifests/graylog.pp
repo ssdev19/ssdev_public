8f66789334045e5d99e3934b94f0cb426fbb40eb
anonss <mysticguy9@yahoo.com>
2022-07-20 11:48:29 -0700

    xx

--- site/profile/manifests/graylog.pp
+++ site/profile/manifests/graylog.pp
@@ -19,6 +19,15 @@ class { 'elastic_stack::repo':
 
 class { 'elasticsearch':
   version     => '7.9.3',
+  api_protocol            => 'http',
+  api_host                => 'localhost',
+  api_port                => 9200,
+  api_timeout             => 10,
+  api_basic_auth_username => undef,
+  api_basic_auth_password => undef,
+  api_ca_file             => undef,
+  api_ca_path             => undef,
+  validate_tls            => true,
   # repo_version => '8.x',
   # ensure => 'absent',
   manage_repo => true,
