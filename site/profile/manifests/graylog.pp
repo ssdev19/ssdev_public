2715a917b1b4006befe163959513d30e2b0f4e43
anonss <mysticguy9@yahoo.com>
2022-07-22 14:11:28 -0700

    as

--- site/profile/manifests/graylog.pp
+++ site/profile/manifests/graylog.pp
@@ -73,7 +73,7 @@ class { 'elasticsearch':
 # config file: /etc/graylog/server/server.conf
 # Password must be at least 16 character long and complex or the service will not start
   class { '::graylog::repository':
-    version => '4.3.3'
+    version => '4.2'
   }
   -> class { '::graylog::server':
       config  => {
