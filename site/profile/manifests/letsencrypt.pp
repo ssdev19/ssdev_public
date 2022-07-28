a13f039f78e881d273a6aca80e382ac2d5698710
anonss <mysticguy9@yahoo.com>
2022-07-28 14:39:10 -0700

    df

--- site/profile/manifests/letsencrypt.pp
+++ site/profile/manifests/letsencrypt.pp
@@ -9,14 +9,14 @@ class { 'letsencrypt':
     server => 'https://acme-v01.api.letsencrypt.org/directory',
   }
 }
-letsencrypt::certonly { $host:
-  domains     => [$fqdn],
-  config_dir  => '/etc/ssl/graylog/',
-  manage_cron          => true,
-  cron_hour            => [0,12],
-  cron_minute          => '30',
-  cron_before_command  => 'service graylog-server stop',
-  cron_success_command => '/bin/systemctl reload graylog-server.service',
-  suppress_cron_output => true,
-}
+# letsencrypt::certonly { $host:
+#   domains     => [$fqdn],
+#   config_dir  => '/etc/ssl/graylog/',
+#   manage_cron          => true,
+#   cron_hour            => [0,12],
+#   cron_minute          => '30',
+#   cron_before_command  => 'service graylog-server stop',
+#   cron_success_command => '/bin/systemctl reload graylog-server.service',
+#   suppress_cron_output => true,
+# }
 }
