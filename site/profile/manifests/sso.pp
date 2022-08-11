c379504c4510c2809e35c8861a024e425554f254
anonss <mysticguy9@yahoo.com>
2022-08-11 08:07:18 -0700

    ss

--- site/profile/manifests/sso.pp
+++ site/profile/manifests/sso.pp
@@ -42,7 +42,7 @@ include 'archive'
 $pingservice = '/etc/systemd/system/pingfederate.service'
 $file_exists = find_file($pingservice)
 if $file_exists {
-notify { "file exists" }
+notify { 'file exists' :}
   } else {
       recursive_file_permissions {'/opt/pingfederate-11.0.2/pingfederate/':
         file_mode => '0775',
