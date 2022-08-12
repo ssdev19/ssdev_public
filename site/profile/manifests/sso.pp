f7bd9705903fe2d1c9e8fe4b397447e775d9b6df
anonss <mysticguy9@yahoo.com>
2022-08-12 14:25:31 -0700

    df

--- site/profile/manifests/sso.pp
+++ site/profile/manifests/sso.pp
@@ -39,19 +39,24 @@ include 'archive'
     ensure => present,
     source => '/tmp/pf-atlassian-cloud-connector/dist/pf-atlassian-cloud-quickconnection-1.0.jar',
   }
-$pingservice = '/etc/systemd/system/pingfederate.service'
-$file_exists = find_file($pingservice)
-if $file_exists {
-  notify{"service exists at ${file_exists}.":}
-} else {
-  notify{"service ${pingservice} does not exists.${file_exists}":}
+  exec { 'chmod -R 775 /opt/pingfederate-11.0.2/pingfederate' :
+    path   => ['/sbin', '/usr/sbin', '/bin'],
+    onlyif => ['test ! -d /etc/systemd/system/pingfederate.service'],
+  }
+
+# $pingservice = '/etc/systemd/system/pingfederate.service'
+# $file_exists = find_file($pingservice)
+# if $file_exists {
+#   notify{"service exists at ${file_exists}.":}
+# } else {
+#   notify{"service ${pingservice} does not exists.${file_exists}":}
       # recursive_file_permissions {'/opt/pingfederate-11.0.2/pingfederate/':
       #   file_mode => '0775',
       #   dir_mode  => '0775',
       #   owner     => $pf_user,
       #   group     => $pf_user,
       #   }
-}
+# }
 
 
   # Copy file needed for Atlassian connector & modify run.properties
