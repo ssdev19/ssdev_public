29f5cf9617d65610fe390f6ecd69ddd9657c7f26
anonss <mysticguy9@yahoo.com>
2022-08-30 15:43:18 -0700

    dd

--- site/profile/manifests/prometheus.pp
+++ site/profile/manifests/prometheus.pp
@@ -27,8 +27,6 @@ $cluster_hide,
         'prober'  => 'icmp',
         'timeout' => '5s',
         'icmp'    => {
-          'valid_status_codes'    => [],
-          # 'method'                => 'GET',
           'preferred_ip_protocol' => 'ip4',
         }
       },
