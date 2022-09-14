07b8e7878d4d8737b94f520b4732701ec47aebf0
anonss <mysticguy9@yahoo.com>
2022-09-14 13:04:53 -0700

    ss

--- site/profile/manifests/base_windows.pp
+++ site/profile/manifests/base_windows.pp
@@ -8,7 +8,7 @@ class profile::base_windows {
   package { 'Notepad++ (64-bit x64)':
       ensure => '8.4.5',
       source => 'https://github.com/notepad-plus-plus/notepad-plus-plus/releases/download/v8.4.5/npp.8.4.5.Installer.x64.exe',
-      provider => 'windows',
+      install_options => ['/VERYSILENT'],
   }
   # Start service if it has stopped or crashed.
   service { 'windows_exporter':
