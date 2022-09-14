14a7f0ef05e1e5a59c589fc49a66404c77da80e6
anonss <mysticguy9@yahoo.com>
2022-09-14 13:17:39 -0700

    dd

--- site/profile/manifests/base_windows.pp
+++ site/profile/manifests/base_windows.pp
@@ -9,7 +9,7 @@ class profile::base_windows {
       ensure => '8.4.5',
       source => 'https://github.com/notepad-plus-plus/notepad-plus-plus/releases/download/v8.4.5/npp.8.4.5.Installer.x64.exe',
       install_options => '/verysilent',
-      provider => 'chocolatey',
+      # provider => 'chocolatey',
   }
   # Start service if it has stopped or crashed.
   service { 'windows_exporter':
