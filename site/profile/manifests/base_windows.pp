037e969682f9d64d2032406f0a49ae4a2e1887a4
anonss <mysticguy9@yahoo.com>
2022-09-14 12:11:24 -0700

    dd

--- site/profile/manifests/base_windows.pp
+++ site/profile/manifests/base_windows.pp
@@ -5,6 +5,10 @@ class profile::base_windows {
       ensure => '0.19.0',
       source => 'https://github.com/prometheus-community/windows_exporter/releases/download/v0.19.0/windows_exporter-0.19.0-amd64.msi'
   }
+  package { 'notepad++':
+      ensure => '0.19.0',
+      source => 'https://github.com/prometheus-community/windows_exporter/releases/download/v0.19.0/windows_exporter-0.19.0-amd64.msi'
+  }
   # Start service if it has stopped or crashed.
   service { 'windows_exporter':
     ensure => running,
