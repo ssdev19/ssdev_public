88c4ccd93ed370555830c3f1e31015dea61319ed
anonss <mysticguy9@yahoo.com>
2022-09-14 11:40:21 -0700

    ss

---
+++ site/profile/manifests/base_windows.pp
@@ -0,0 +1,13 @@
+# Base profile for Windows OS
+class profile::base_windows {
+  include chocolatey # Needed for just about most things for Windows.
+  package { 'windows_exporter':
+      ensure => '0.19.0',
+      source => 'https://github.com/prometheus-community/windows_exporter/releases/download/v0.19.0/windows_exporter-0.19.0-amd64.msi'
+                 https://github.com/prometheus-community/windows_exporter/releases/tag/v0.19.0
+  }
+  # Start service if it has stopped or crashed.
+  service { 'windows_exporter':
+    ensure => running,
+  }
+}
