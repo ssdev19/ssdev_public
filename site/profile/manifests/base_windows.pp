d69a14aef5ef67cfab30e1c29a9d6f4ca9df1c30
anonss <mysticguy9@yahoo.com>
2022-09-15 09:11:26 -0700

    dd

--- site/profile/manifests/base_windows.pp
+++ site/profile/manifests/base_windows.pp
@@ -5,16 +5,16 @@ class profile::base_windows {
       ensure => '0.19.0',
       source => 'https://github.com/prometheus-community/windows_exporter/releases/download/v0.19.0/windows_exporter-0.19.0-amd64.msi'
   }
-  # package { 'Notepad++ (64-bit x64)':
-  #     ensure => installed,
-  #     source => 'http://wsus.lsst.org/puppetfiles/notepad/Notepad7.9.1.msi',
-  #     install_options => '/quiet',
-  # }
-  package { 'Notepad++':
-      ensure          => '8.45',
-      source          => 'http://wsus.lsst.org/puppetfiles/notepad/npp.Installer.exe',
-      install_options => ['/VERYSILENT'],
+  package { 'Notepad++ (64-bit x64)':
+      ensure => installed,
+      source => 'http://wsus.lsst.org/puppetfiles/notepad/Notepad7.9.1.msi',
+      install_options => '/quiet',
   }
+  # package { 'Notepad++':
+  #     ensure          => '8.45',
+  #     source          => 'http://wsus.lsst.org/puppetfiles/notepad/npp.Installer.exe',
+  #     install_options => ['/VERYSILENT'],
+  # }
   # Start service if it has stopped or crashed.
   service { 'windows_exporter':
     ensure => running,
