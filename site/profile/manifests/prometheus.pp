cd2bdb040bc84057047306677b992043736fc535
anonss <mysticguy9@yahoo.com>
2022-11-04 14:33:25 -0700

    dd

--- site/profile/manifests/prometheus.pp
+++ site/profile/manifests/prometheus.pp
@@ -8,7 +8,6 @@ $cluster_hide,
 # Firewall rules are in private repo
   include prometheus
   include prometheus::synology_prometheus
-  include prometheus::snmp_exporter
   class { 'prometheus::blackbox_exporter':
     version => '0.22.0',
     modules => {
