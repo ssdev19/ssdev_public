7f52059b9ea88658bb9a6cee273d36127b7ad1d6
anonss <mysticguy9@yahoo.com>
2023-01-27 13:00:42 -0700

    1.4

--- site/profile/manifests/base_linux.pp
+++ site/profile/manifests/base_linux.pp
@@ -34,7 +34,7 @@ class profile::base_linux (
 
 # config: /etc/systemd/system/node_exporter.service
   class { 'prometheus::node_exporter':
-    version       => '1.4.0-rc.0',
+    version       => '1.5.0-rc.0',
     extra_options => '--collector.systemd \--collector.processes \--collector.meminfo_numa',
   }
   #   $fqdn = $::facts['networking']['fqdn']
