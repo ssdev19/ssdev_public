1346cd11638ac6d22d38e5e1f6f010feb6e138d1
anonss <mysticguy9@yahoo.com>
2023-01-31 11:52:34 -0700

    ss

--- site/profile/manifests/grayjava.pp
+++ site/profile/manifests/grayjava.pp
@@ -3,6 +3,8 @@ class profile::grayjava {
   java::adopt { 'jdk11' :
     ensure  => 'latest',
     version => '11',
+    version_major => '2',
+    version_minor => '9',
     java    => 'jdk',
   }
 }
