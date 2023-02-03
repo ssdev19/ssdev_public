6f193b3bb5604d6938dbae9b08c3378703f52af9
anonss <mysticguy9@yahoo.com>
2023-02-03 13:57:09 -0700

    df

--- site/profile/manifests/base_linux.pp
+++ site/profile/manifests/base_linux.pp
@@ -145,7 +145,10 @@ if $awscli {
 $file_path = '/tmp/test_file'
 
 $file_exists = find_file($file_path)
+
 if $file_exists  {
   notify{"File ${file_path} exist":}
+} else {
+  notify{"File ${file_path} does not exist":}
 }
 }
