672253a6bd61df60d6a6e2b32a4da3c4d199f356
anonss <mysticguy9@yahoo.com>
2023-02-06 13:45:59 -0700

    d

--- site/facts/facts.d/test1.rb
+++ site/facts/facts.d/test1.rb
@@ -1,5 +1,5 @@
 #
-if FileTest.directory?("/opt/pingfederate-11.0.2/pingfederate/server/defaults")
+if FileTest.directory?("/opt/pingfederate-11.0.2/pingfederate/server/default")
     Facter.add("test1") do
         setcode { true }
     end
