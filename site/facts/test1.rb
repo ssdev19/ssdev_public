757b109d284055534fb3b7ffe54afdaa5ccb5b3f
anonss <mysticguy9@yahoo.com>
2023-02-06 12:39:41 -0700

    ss

--- site/facts/test1.rb
+++ site/facts/test1.rb
@@ -1,4 +1,4 @@
-if((test-path '/opt/pingfederate-11.0.2/pingfederate/server/defaulte') -or (test-path '/opt/pingfederate-11.0.2/pingfederate/servere')){
+if((test-path '/opt/pingfederate-11.0.2/pingfederate/server/default') -or (test-path '/opt/pingfederate-11.0.2/pingfederate/server')){
     write-host "test_file_exists=true
 }
 else{
