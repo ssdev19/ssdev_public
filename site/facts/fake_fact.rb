10647733fbd91ccd8ed27219ffd30a7f4e098174
anonss <mysticguy9@yahoo.com>
2023-02-08 15:22:40 -0700

    s

--- site/facts/fake_fact.rb
+++ site/facts/fake_fact.rb
@@ -1,5 +1,5 @@
 Facter.add('hardware_platform') do
     setcode do
-      Facter::Core::Execution.execute('/bin/uname --hardware-platform')
+      Facter::Core::Execution.execute('/bin/uname --hardware-platformd')
     end
   end
