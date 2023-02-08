6b1e737e988509b328adea9afa378f11fe9a81c8
anonss <mysticguy9@yahoo.com>
2023-02-08 09:30:12 -0700

    s

--- site/facts/facts.d/test1.rb
+++ site/facts/facts.d/test1.rb
@@ -2,7 +2,7 @@
 
 Facter.add('test1') do
     setcode do
-        ! Dir.glob("/opt/pingfederate-11.0.2/pingfederate/server/default").empty?
+        ! Dir.glob("/opt/pingfederate-11.0.2/pingfederate/server/defaulta").empty?
     end
    end
 
