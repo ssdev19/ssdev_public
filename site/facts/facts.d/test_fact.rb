4867fe63d10bd62fc27c6c27178d9299d8bc428c
anonss <mysticguy9@yahoo.com>
2023-02-08 14:35:58 -0700

    sd

--- site/facts/facts.d/test_fact.rb
+++ site/facts/facts.d/test_fact.rb
@@ -5,3 +5,12 @@ Facter.add('test_fact') do
         if File.exist? '/tmp/testfile'
     end
    end
+
+   # hardware_platform.rb
+
+Facter.add('hardware_platform') do
+    setcode do
+      Facter::Core::Execution.execute('/bin/uname --hardware-platform')
+    end
+  end
+  
