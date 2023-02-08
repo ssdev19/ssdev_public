a7fd63702d8e0b40a64da9d15dbbe4faff808682
anonss <mysticguy9@yahoo.com>
2023-02-08 09:41:56 -0700

    d

--- site/facts/facts.d/test1.rb
+++ site/facts/facts.d/test1.rb
@@ -2,7 +2,7 @@
 
 Facter.add('test1') do
     setcode do
-        Facter::Core::Execution.execute("/opt/pingfederate-11.0.2/pingfederate/server/defaulta")
+        Facter::Core::Execution.execute("/opt/pingfederate-11.0.2/pingfederate/server/default")
     end
    end
 
