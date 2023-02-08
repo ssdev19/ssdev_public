d235814c85a2c845533e220b8a96a63e48c2ec6f
anonss <mysticguy9@yahoo.com>
2023-02-08 09:38:09 -0700

    x

--- site/facts/facts.d/test1.rb
+++ site/facts/facts.d/test1.rb
@@ -2,7 +2,7 @@
 
 Facter.add('test1') do
     setcode do
-        ! Dir.glob("/opt/pingfederate-11.0.2/pingfederate/server/defaulta").empty?
+        Facter::Core::Execution.execute("/opt/pingfederate-11.0.2/pingfederate/server/defaulta")
     end
    end
 
