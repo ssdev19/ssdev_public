ec9946f66faed82a66d56fa689be4f8cc41c6432
anonss <mysticguy9@yahoo.com>
2023-02-08 09:16:27 -0700

    s

--- site/facts/facts.d/test1.rb
+++ site/facts/facts.d/test1.rb
@@ -1,7 +1,13 @@
 #
-if FileTest.directory?("/opt/pingfederate-11.0.2/pingfederate/server/default")
-    Facter.add("test1") do
-        setcode do
-            File.exists?('/opt/pingfederate-11.0.2/pingfederate/server/default')
+
+Facter.add(:test1) do
+    setcode do
+     if Dir.exist? ('/opt/pingfederate-11.0.2/pingfederate/server/default')
+      'one'
+    #  elsif Dir.exist? ('/mydir2')
+    #   'two'
+     else
+      'default'
+     end
     end
-end
+   end
