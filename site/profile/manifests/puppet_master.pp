919bd719ee4a3d2d24895edb2d0144a8fd21b591
anonss <mysticguy9@yahoo.com>
2023-02-08 14:25:14 -0700

    dd

--- site/profile/manifests/puppet_master.pp
+++ site/profile/manifests/puppet_master.pp
@@ -8,7 +8,7 @@ class profile::puppet_master {
       provider => 'puppetserver_gem',
     }
 
-if $facts['test_fact'] == true {
+if $facts['test_fact'] == false {
     notify{"Path exist":}
     } else {
       notify{"File does not existss":}
