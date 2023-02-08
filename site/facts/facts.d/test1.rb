910eb8111838b35ebbd34e645a3d9b362449f03f
anonss <mysticguy9@yahoo.com>
2023-02-08 10:36:04 -0700

    ss

--- site/facts/facts.d/test1.rb
+++ site/facts/facts.d/test1.rb
@@ -2,7 +2,7 @@
 
 Facter.add(:test1) do
     setcode do
-        File.exists?("/opt/pingfederate-11.0.2/pingfederate/server/default")
+        File.exists?('/opt/pingfederate-11.0.2/pingfederate/server/default')
     end
    end
 
