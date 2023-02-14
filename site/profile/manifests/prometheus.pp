b3944e6d058c821f48b41448cda2b8fa972157c1
anonss <mysticguy9@yahoo.com>
2023-02-14 12:51:13 -0700

    s

--- site/profile/manifests/prometheus.pp
+++ site/profile/manifests/prometheus.pp
@@ -93,6 +93,6 @@ $gmail_account = lookup('gmail_account')
   }
     exec { 'Allow any user to use icmp ':
     path    => [ '/usr/bin', '/bin', '/usr/sbin' ],
-    command => "sysctl net.ipv4.ping_group_range= 0 2147483647",
+    command => 'sysctl net.ipv4.ping_group_range= 0 21474',
   }
 }
