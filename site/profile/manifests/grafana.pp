115840d1c7882bdaf79ef7b5d804e9ad57949911
anonss <mysticguy9@yahoo.com>
2023-02-16 12:43:12 -0700

    d

--- site/profile/manifests/grafana.pp
+++ site/profile/manifests/grafana.pp
@@ -22,9 +22,6 @@ class profile::grafana {
     },
     'server'    => {
       http_port => 3000,
-      domain    => 'lsst.org',
-      root_url  => 'https://grafana.us.lsst.org:3000',
-      enforce_domain => False,
       cert_key  => '/etc/grafana/grafana.key',
       cert_file => '/etc/grafana/grafana.crt',
       protocol  => 'https',
