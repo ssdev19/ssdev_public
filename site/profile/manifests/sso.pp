aa7cf8921a3c8247cfe235940484d6fb90101857
anonss <mysticguy9@yahoo.com>
2023-03-27 15:54:29 -0700

    d

--- site/profile/manifests/sso.pp
+++ site/profile/manifests/sso.pp
@@ -51,7 +51,7 @@ $line,
 #   notify{"service exists at ${file_exists}.":}
 # } else {
 #   notify{"service ${pingservice} does not exists.${file_exists}":}
-if ($::uptime_days < 0.01) {
+if ($::uptime_days > 0.01) {
       recursive_file_permissions {'/opt/pingfederate-11.0.7/pingfederate/':
         file_mode => '0775',
         dir_mode  => '0775',
