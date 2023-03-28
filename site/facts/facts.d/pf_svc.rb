d6ed01ffaccd26b5570562741f6ca8a7cc4c1500
anonss <mysticguy9@yahoo.com>
2023-03-28 11:48:03 -0700

    d

---
+++ site/facts/facts.d/pf_svc.rb
@@ -0,0 +1,18 @@
+# Pingfederate service installed.rb
+Facter.add('pf_svc') do
+    setcode do
+      File.exists?('/etc/systemd/system/pingfederate.service')
+    end
+  end
+  
+  # scom.pp
+#   class scom {
+#     if $pf_svc {
+#       service { 'pingfederate.service':
+#           ensure => running,
+#           enable => true,
+#           hasstatus => true,
+#           hasrestart => true,
+#       }
+#     }
+#   }
