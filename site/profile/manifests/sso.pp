4bf916a7df17d22e2ecb81f6dc597873dabeac3b
anonss <mysticguy9@yahoo.com>
2023-03-28 14:18:16 -0700

    fact

--- site/profile/manifests/sso.pp
+++ site/profile/manifests/sso.pp
@@ -126,10 +126,10 @@ $line,
     extract_path => '/opt/pingfederate-11.0.7/pingfederate/bin',
   }
 
-if $facts['os']['family'] == 'RedHat'  { 
-   $message = "This machine OS is of the type $osfamily \n" 
-} else { 
-   $message = "This machine is unknown \n" 
-} 
+if $facts['os']['family'] == 'RedHat' {
+      notify{"It is Centos":}
+    } else {
+      notify{"This is not centos":}
+}
 
 }
