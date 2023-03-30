c72c6b2b08ceb2e58d2de2762e8371d97054fef6
anonss <mysticguy9@yahoo.com>
2023-03-30 08:11:51 -0700

    dd

--- site/profile/lib/facter/hardware_platform.rb
+++ site/profile/lib/facter/hardware_platform.rb
@@ -6,3 +6,16 @@ Facter.add('hardware_platform') do
     end
   end
   
+Facter.add(:osfamily) do
+  setcode do
+    distid = Facter.value(:lsbdistid)
+    case distid
+    when /RedHatEnterprise|CentOS|Fedora/
+      'redhat'
+    when 'ubuntu'
+      'debian'
+    else
+      distid
+    end
+  end
+end
