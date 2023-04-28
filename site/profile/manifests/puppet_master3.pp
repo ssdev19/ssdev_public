f714d188bf34090933d36652837bc3f3f573cb98
anonss <mysticguy9@yahoo.com>
2023-04-28 12:17:58 -0700

    sd

--- site/profile/manifests/puppet_master3.pp
+++ site/profile/manifests/puppet_master3.pp
@@ -7,7 +7,7 @@ class profile::puppet_master3 {
   include foreman::plugin::tasks
   include foreman::plugin::templates
   # include foreman_proxy
-  include foreman_proxy::plugin::remote_execution::script
+  # include foreman_proxy::plugin::remote_execution::script
   include foreman::repo
   include puppet
   yumrepo { 'pc_repo':
