0bf7c21d9dc2897aae860486154d68e0eadb0f74
anonss <mysticguy9@yahoo.com>
2023-07-18 11:59:59 -0700

    sd

---
+++ site/profile/manifests/yourl.pp
@@ -0,0 +1,13 @@
+# 
+class profile::yourls {
+  archive { '/tmp/yourls-1.9.2.zip':
+    ensure   => present,
+    source   => 'https://github.com/YOURLS/YOURLS/archive/refs/tags/1.9.2.zip',
+    provider => 'wget',
+    cleanup  => false,
+  }
+  # file { '/opt/tomcat/webapps/ROOT.war':
+  #   ensure => present,
+  #   source => '/tmp/pwm-1.9.2.war',
+  # }
+}
