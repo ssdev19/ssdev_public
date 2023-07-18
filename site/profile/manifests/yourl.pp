f43f41a927166b018d96b0695ad560978d617d0d
anonss <mysticguy9@yahoo.com>
2023-07-18 12:09:56 -0700

    dd

--- site/profile/manifests/yourl.pp
+++ site/profile/manifests/yourl.pp
@@ -3,7 +3,7 @@ class profile::yourls {
   archive { '/tmp/yourls-1.9.2.zip':
     ensure   => present,
     source   => 'https://github.com/YOURLS/YOURLS/archive/refs/tags/1.9.2.zip',
-    # extract_path => ,
+    extract_path => ,
     cleanup  => false,
   }
   # file { '/opt/tomcat/webapps/ROOT.war':
