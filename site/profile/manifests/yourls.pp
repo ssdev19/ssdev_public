166ce6d24343cc11a6681c5c1016e910ac238978
anonss <mysticguy9@yahoo.com>
2023-07-19 07:42:31 -0700

    d

--- site/profile/manifests/yourls.pp
+++ site/profile/manifests/yourls.pp
@@ -3,18 +3,18 @@ class profile::yourls {
   include nginx
   include '::php'
 
-  # unless $::yourls_config  {
-  # archive { '/tmp/yourls-1.9.2.zip':
-  #   ensure       => present,
-  #   source       => 'https://github.com/YOURLS/YOURLS/archive/refs/tags/1.9.2.zip',
-  #   extract_path => '/var/www',
-  #   extract      => true,
-  #   provider     => 'wget',
-  #   cleanup      => false,
-  # }
-  # file { '/var/www/YOURLS-1.9.2/user/config.php':
-  #           ensure => present,
-  #           source => '/var/www/YOURLS-1.9.2/user/config-sample.php',
-  # }
-  # }
+  unless $::yourls_config  {
+  archive { '/tmp/yourls-1.9.2.zip':
+    ensure       => present,
+    source       => 'https://github.com/YOURLS/YOURLS/archive/refs/tags/1.9.2.tar.gz',
+    extract_path => '/etc/nginx',
+    extract      => true,
+    provider     => 'wget',
+    cleanup      => false,
+  }
+  file { '/etc/nginx/YOURLS-1.9.2/user/config.php':
+            ensure => present,
+            source => '/etc/nginx/YOURLS-1.9.2/user/config-sample.php',
+  }
+  }
 }
