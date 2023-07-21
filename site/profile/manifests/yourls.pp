2c95108d384c7200ee24b9b79a383c70dfb6171e
anonss <mysticguy9@yahoo.com>
2023-07-20 19:36:09 -0700

    s

--- site/profile/manifests/yourls.pp
+++ site/profile/manifests/yourls.pp
@@ -5,7 +5,7 @@ $yourls_version,
 $yourls_site,
 
 ){
-  # include nginx
+  include nginx
   include mysql::server
   include '::php'
 
@@ -60,29 +60,5 @@ file { '/etc/nginx/YOURLS':
   ensure => 'link',
   target => "/etc/nginx/YOURLS-${yourls_version}",
 }
-nginx::resource::server { 'puppet':
-  ensure               => present,
-  server_name          => ['puppet'],
-  listen_port          => 8140,
-  ssl                  => true,
-  ssl_cert             => '/var/lib/puppet/ssl/certs/example.com.pem',
-  ssl_key              => '/var/lib/puppet/ssl/private_keys/example.com.pem',
-  ssl_port             => 8140,
-  server_cfg_append    => {
-    'passenger_enabled'      => 'on',
-    'passenger_ruby'         => '/usr/bin/ruby',
-    'ssl_crl'                => '/var/lib/puppet/ssl/ca/ca_crl.pem',
-    'ssl_client_certificate' => '/var/lib/puppet/ssl/certs/ca.pem',
-    'ssl_verify_client'      => 'optional',
-    'ssl_verify_depth'       => 1,
-  },
-  www_root             => '/etc/puppet/rack/public',
-  use_default_location => false,
-  access_log           => '/var/log/nginx/puppet_access.log',
-  error_log            => '/var/log/nginx/puppet_error.log',
-  passenger_cgi_param  => {
-    'HTTP_X_CLIENT_DN'     => '$ssl_client_s_dn',
-    'HTTP_X_CLIENT_VERIFY' => '$ssl_client_verify',
-  },
-}
+
 }
