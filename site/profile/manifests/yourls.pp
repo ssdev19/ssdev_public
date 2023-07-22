16f00ba318ec42c27bc4132ae056306feb9b931e
anonss <mysticguy9@yahoo.com>
2023-07-22 11:50:47 -0700

    d

--- site/profile/manifests/yourls.pp
+++ site/profile/manifests/yourls.pp
@@ -104,7 +104,7 @@ file { '/etc/nginx/YOURLS':
 #   provider => 'shell',
 # }
 
-  unless $::yourls_db  {
+  if $::yourls_db  {
     mysql::db { $yourls_db_name:
       user            => $yourls_db_user,
       password        => $yourls_db_pass,
