ff19acd5ccca0d16d94529b57f466d58ce0f820f
anonss <mysticguy9@yahoo.com>
2023-07-22 10:39:39 -0700

    df

--- site/profile/manifests/yourls.pp
+++ site/profile/manifests/yourls.pp
@@ -104,14 +104,14 @@ file { '/etc/nginx/YOURLS':
     cleanup => false,
   }
 
-# mysql::db { $yourls_db_name:
-#   user            => $yourls_db_user,
-#   password        => $yourls_db_pass,
-#   host            => 'localhost',
-#   grant           => ['SELECT', 'UPDATE'],
-#   sql             => ['/tmp/mysql-db-yourls.gz'],
-#   import_cat_cmd  => 'zcat',
-#   import_timeout  => 900,
-#   # mysql_exec_path => '/opt/rh/rh-myql57/root/bin',
-# }
+mysql::db { $yourls_db_name:
+  user            => $yourls_db_user,
+  password        => $yourls_db_pass,
+  host            => 'localhost',
+  grant           => ['SELECT', 'UPDATE'],
+  sql             => ['/tmp/mysql-db-yourls.gz'],
+  import_cat_cmd  => 'zcat',
+  import_timeout  => 900,
+  # mysql_exec_path => '/opt/rh/rh-myql57/root/bin',
+}
 }
