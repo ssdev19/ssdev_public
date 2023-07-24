b0cc51dec55f7088c34e71b4c24056ee5739fc15
anonss <mysticguy9@yahoo.com>
2023-07-24 13:11:08 -0700

    f

--- site/profile/manifests/yourls.pp
+++ site/profile/manifests/yourls.pp
@@ -125,12 +125,12 @@ file { '/etc/nginx/YOURLS':
   #     include     => ['fastcgi.conf'],
   # }
 
-  # exec {'compile':
-  #   path     => [ '/usr/bin', '/bin', '/usr/sbin' ],
-  #   cwd      => '/tmp/nginx-1.24.0/',
-  #   provider => shell,
-  #   command  => "./configure --prefix=/etc/nginx --sbin-path=/usr/sbin/nginx --error-log-path=/var/log/nginx/error.log --pid-path=/var/run/nginx.pid --lock-path=/var/run/nginx.lock --user=nginx --group=nginx --add-module=./nginx-auth-ldap",
-  # }
+  exec {'compile':
+    path     => [ '/usr/bin', '/bin', '/usr/sbin' ],
+    cwd      => '/tmp/nginx-1.24.0/',
+    provider => shell,
+    command  => "./configure --prefix=/etc/nginx --sbin-path=/usr/sbin/nginx --error-log-path=/var/log/nginx/error.log --pid-path=/var/run/nginx.pid --lock-path=/var/run/nginx.lock --user=nginx --group=nginx --add-module=./nginx-auth-ldap",
+  }
   archive { '/tmp/mysql-db-yourls.gz' :
     ensure  => present,
     source  => 's3://yourls-data/mysql-db-yourls-202303020300.gz',
