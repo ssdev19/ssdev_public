81e840f2ac00f3389015e16d36c06ecb9d261f21
anonss <mysticguy9@yahoo.com>
2023-07-25 07:20:14 -0700

    zx

--- site/profile/manifests/yourls.pp
+++ site/profile/manifests/yourls.pp
@@ -73,7 +73,7 @@ file { '/etc/nginx/YOURLS':
     path     => [ '/usr/bin', '/bin', '/usr/sbin' ],
     cwd      => '/tmp/nginx-1.24.0/',
     provider => shell,
-    command  => './configure --prefix=/etc/nginx --sbin-path=/usr/sbin/nginx --error-log-path=/var/log/nginx/error.log --pid-path=/var/run/nginx.pid --lock-path=/var/run/nginx.lock --user=nginx --group=nginx --add-module=./nginx-auth-ldap',
+    command  => './configure --prefix=/etc/nginx --sbin-path=/usr/sbin/nginx --error-log-path=/var/log/nginx/error.log --pid-path=/var/run/nginx.pid --lock-path=/var/run/nginx.lock --user=nginx --group=nginx --add-module=./nginx-auth-ldap; make; make install',
   }
   archive { '/tmp/mysql-db-yourls.gz' :
     ensure  => present,
