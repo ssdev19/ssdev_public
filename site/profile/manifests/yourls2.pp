13f6daf36de94e933006ba98a3c2dc1e0f77ce0b
anonss <mysticguy9@yahoo.com>
2023-07-31 08:26:44 -0700

    d

---
+++ site/profile/manifests/yourls2.pp
@@ -0,0 +1,58 @@
+class profile::yourls ( String
+
+$yourls_version,
+$yourls_site,
+
+
+){
+  # include nginx
+  include mysql::server
+  # include '::php'
+  # include '::php::globals'
+
+  Package { [ 'openldap-devel', 'make' ]:
+    ensure => installed,
+  }
+$yourls_user_passwords = lookup('yourls_user_passwords')
+$yourls_db_pass = lookup('yourls_db_pass')
+$yourls_db_user = lookup('yourls_db_user')
+$yourls_db_name = lookup('yourls_db_name')
+  archive { '/tmp/mysql-db-yourls.gz' :
+    ensure  => present,
+    source  => 's3://yourls-data/yourls/20230728030002-mysql-db-yourls.gz',
+    cleanup => false,
+  }
+  if $::yourls_db  {
+    mysql::db { $yourls_db_name:
+      user           => $yourls_db_user,
+      password       => $yourls_db_pass,
+      host           => 'localhost',
+      grant          => ['SELECT', 'UPDATE'],
+      sql            => ['/tmp/mysql-db-yourls.gz'],
+      import_cat_cmd => 'zcat',
+      import_timeout => 900,
+      # mysql_exec_path => '/opt/rh/rh-myql57/root/bin',
+    }
+}
+
+
+    archive { '/tmp/nginx-1.24.0.tar.gz':
+      ensure       => present,
+      source       => 'http://nginx.org/download/nginx-1.24.0.tar.gz',
+      extract_path => '/tmp/',
+      extract      => true,
+      provider     => 'wget',
+      cleanup      => true,
+    }
+
+    vcsrepo { '/tmp/nginx-1.24.0/nginx-auth-ldap':
+      ensure   => present,
+      provider => git,
+      source   => 'https://github.com/kvspb/nginx-auth-ldap.git',
+      user     => 'root',
+    }
+  file { '/etc/nginx/YOURLS':
+    ensure => 'link',
+    target => "/etc/nginx/YOURLS-${yourls_version}",
+  }
+}
