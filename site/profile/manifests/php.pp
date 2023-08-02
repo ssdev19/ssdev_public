add1ffbb02d30090fcfa0726c002f2449e02420d
anonss <mysticguy9@yahoo.com>
2023-08-02 16:31:08 -0700

    df

--- site/profile/manifests/php.pp
+++ site/profile/manifests/php.pp
@@ -4,8 +4,8 @@ class profile::php {
     ensure => installed,
   }
 class { '::php::globals':
-  php_version => '7.4',
-  config_root => '/etc/php/7.4',
+  php_version => '8.1',
+  config_root => '/etc/php/8.1',
 }
   -> class { '::php':
       manage_repos => true,
