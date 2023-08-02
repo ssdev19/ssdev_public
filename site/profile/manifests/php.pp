3ef54a55f5a741c8b896f53099a20086347fa3b1
anonss <mysticguy9@yahoo.com>
2023-08-02 14:41:55 -0700

    e

--- site/profile/manifests/php.pp
+++ site/profile/manifests/php.pp
@@ -2,8 +2,8 @@
 class profile::php {
 
 class { '::php::globals':
-  php_version => '8.0',
-  config_root => '/etc/php/8.0',
+  php_version => '8.1',
+  config_root => '/etc/php/8.1',
 }
   -> class { '::php':
       manage_repos => true
