38026d858a70bdd3d4eab088fe03c935d33a76db
anonss <mysticguy9@yahoo.com>
2023-08-07 10:26:18 -0700

    s

--- site/profile/manifests/yourls2.pp
+++ site/profile/manifests/yourls2.pp
@@ -36,60 +36,21 @@ $yourls_db_name = lookup('yourls_db_name')
 }
 
 
-    archive { '/tmp/nginx-1.14.1.tar.gz':
+    archive { '/tmp/nginx-1.22.1.tar.gz':
       ensure       => present,
-      source       => 'http://nginx.org/download/nginx-1.14.1.tar.gz',
+      source       => 'http://nginx.org/download/nginx-1.22.1.tar.gz',
       extract_path => '/tmp/',
       extract      => true,
       provider     => 'wget',
       cleanup      => true,
     }
 
-    vcsrepo { '/tmp/nginx-1.14.1/nginx-auth-ldap':
+    vcsrepo { '/tmp/nginx-1.22.1/nginx-auth-ldap':
       ensure   => present,
       provider => git,
       source   => 'https://github.com/kvspb/nginx-auth-ldap.git',
       user     => 'root',
     }
-  file { '/etc/nginx/YOURLS':
-    ensure => 'link',
-    target => "/etc/nginx/YOURLS-${yourls_version}",
-  }
-    archive { "/tmp/yourls-${yourls_version}.tar.gz":
-      ensure       => present,
-      source       => "https://github.com/YOURLS/YOURLS/archive/refs/tags/${yourls_version}.tar.gz",
-      extract_path => '/etc/nginx',
-      extract      => true,
-      provider     => 'wget',
-      cleanup      => false,
-    }
-    file { "/etc/nginx/YOURLS-${yourls_version}/shorten":
-    ensure => directory,
-    }
-  archive { "/etc/nginx/YOURLS-${yourls_version}/shorten/index.php" :
-    ensure  => present,
-    source  => 's3://yourls-data/index.php',
-    cleanup => false,
-  }
-  archive { "/etc/nginx/YOURLS-${yourls_version}/index.html" :
-    ensure  => present,
-    source  => 's3://yourls-data/index.html',
-    cleanup => false,
-  }
-  archive { '/etc/nginx/conf.d/yourls.conf' :
-    ensure  => present,
-    source  => 's3://yourls-data/yourls_config_new.txt',
-    cleanup => false,
-  }
-  archive { '/etc/pki/tls/certs/ls.st.current.crt' :
-    ensure  => present,
-    source  => 's3://yourls-data/ls.st.current.crt',
-    cleanup => false,
-  }
-  archive { '/etc/pki/tls/certs/ls.st.current.key' :
-    ensure  => present,
-    source  => 's3://yourls-data/ls.st.current.key',
-    cleanup => false,
-  }
+
 
 }
