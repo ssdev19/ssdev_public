90a7689dd74d0f678fd3e8e761b37ff2919d41ab
anonss <mysticguy9@yahoo.com>
2023-08-14 08:01:27 -0700

    s

--- site/profile/manifests/yourls.pp
+++ site/profile/manifests/yourls.pp
@@ -48,7 +48,8 @@ $yourls_db_name = lookup('yourls_db_name')
       path     => [ '/usr/bin', '/bin', '/usr/sbin' ],
       cwd      => '/usr/src/nginx-1.22.1/',
       provider => shell,
-      command  => './configure --prefix=/etc/nginx --sbin-path=/usr/sbin/nginx --modules-path=/usr/lib64/nginx/modules --conf-path=/etc/nginx/nginx.conf --error-log-path=/var/log/nginx/error.log --pid-path=/var/run/nginx.pid --lock-path=/var/run/nginx.lock --user=nginx --group=nginx --with-threads --with-http_ssl_module --with-http_v2_module --http-log-path=/var/log/nginx/access.log --add-module=/usr/src/nginx-1.22.1/nginx-auth-ldap; make install',
+      # command  => './configure --prefix=/etc/nginx --sbin-path=/usr/sbin/nginx --modules-path=/usr/lib64/nginx/modules --conf-path=/etc/nginx/nginx.conf --error-log-path=/var/log/nginx/error.log --pid-path=/var/run/nginx.pid --lock-path=/var/run/nginx.lock --user=nginx --group=nginx --with-threads --with-http_ssl_module --with-http_v2_module --http-log-path=/var/log/nginx/access.log --add-module=/usr/src/nginx-1.22.1/nginx-auth-ldap; make install',
+      command  => './configure --add-module=/usr/src/nginx-1.22.1/nginx-auth-ldap; make install',
     }
   }
   unless $::yourls_config  {
