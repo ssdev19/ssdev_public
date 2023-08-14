415c73510899b53d9c2cdb0026fce935ca6491c9
anonss <mysticguy9@yahoo.com>
2023-08-14 16:55:28 -0700

    df

--- site/profile/manifests/php.pp
+++ site/profile/manifests/php.pp
@@ -7,13 +7,13 @@ class profile::php {
   class { 'php::repo::redhat':
     yum_repo => 'remi_php81'
   }
-  class { '::php::globals':
-    php_version => '8.0',
-    # config_root => '/etc/php/8.0',
-  }
-  -> class { '::php':
-      manage_repos => true
-    }
+  # class { '::php::globals':
+  #   php_version => '8.0',
+  #   # config_root => '/etc/php/8.0',
+  # }
+  # -> class { '::php':
+  #     manage_repos => true
+  #   }
 
   # class { 'php::packages':
   #   ensure => '8.2.8',
