e545114b7a36ba563538dd5560abaee3b057fd50
anonss <mysticguy9@yahoo.com>
2023-08-14 13:02:28 -0700

    df

--- site/profile/manifests/php.pp
+++ site/profile/manifests/php.pp
@@ -3,9 +3,9 @@ class profile::php {
   Package { [ 'openldap-devel', 'make', 'yum-utils', 'epel-release' ]:
     ensure => installed,
   }
-  class { '::php::globals':
-    php_version => '8.1',
-    config_root => '/etc/php/8.1',
+  class { 'php::packages':
+    ensure => '8.1',
+    # config_root => '/etc/php/8.1',
   }
   -> class { '::php':
       manage_repos => true,
