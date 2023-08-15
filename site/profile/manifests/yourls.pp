8375dc5760951c804f455ba5e00bb261bd19b1ca
anonss <mysticguy9@yahoo.com>
2023-08-15 13:24:18 -0700

    df

--- site/profile/manifests/yourls.pp
+++ site/profile/manifests/yourls.pp
@@ -217,15 +217,6 @@ file { '/etc/nginx/YOURLS':
 #   enable    => true,
 #   ensure    => 'running',
 #   }
-  unless $::yourls_config  {
-    exec {'compile':
-      path     => [ '/usr/bin', '/bin', '/usr/sbin' ],
-      cwd      => "/usr/src/nginx-${nginx_version}/",
-      provider => shell,
-      command  => "./configure --prefix=/usr/share/nginx --sbin-path=/usr/sbin/nginx --modules-path=/usr/lib64/nginx/modules --conf-path=/etc/nginx/nginx.conf --error-log-path=/var/log/nginx/error.log --http-log-path=/var/log/nginx/access.log --http-client-body-temp-path=/var/lib/nginx/tmp/client_body --http-proxy-temp-path=/var/lib/nginx/tmp/proxy --http-fastcgi-temp-path=/var/lib/nginx/tmp/fastcgi --http-uwsgi-temp-path=/var/lib/nginx/tmp/uwsgi --http-scgi-temp-path=/var/lib/nginx/tmp/scgi --pid-path=/run/nginx.pid --lock-path=/run/lock/subsys/nginx --user=nginx --group=nginx --with-file-aio --with-http_ssl_module --with-http_v2_module --with-http_realip_module --with-stream_ssl_preread_module --with-http_addition_module --with-http_sub_module --with-http_dav_module --with-http_flv_module --with-http_mp4_module --with-http_gunzip_module --with-http_gzip_static_module --with-http_random_index_module --with-http_secure_link_module --with-http_degradation_module --with-http_slice_module --with-http_stub_status_module --with-http_auth_request_module --with-mail=dynamic --with-mail_ssl_module --with-pcre --with-pcre-jit --with-stream=dynamic --with-stream_realip_module --with-stream_ssl_module --add-module=/usr/src/nginx-${nginx_version}/nginx-auth-ldap; make; make install",
-      timeout  => 900,
-    }
-  }
   file {
     '/etc/systemd/system/nginx.service.d':
       ensure => directory,
@@ -235,6 +226,15 @@ file { '/etc/nginx/YOURLS':
       path     => [ '/usr/bin', '/bin', '/usr/sbin' ],
       provider => shell,
       command  => 'printf "[Service]\\nExecStartPost=/bin/sleep 0.1\\n" > /etc/systemd/system/nginx.service.d/override.conf; systemctl daemon-reload; systemctl restart nginx ',
+    }
+  }
+  unless $::yourls_config  {
+    exec {'compile':
+      path     => [ '/usr/bin', '/bin', '/usr/sbin' ],
+      cwd      => "/usr/src/nginx-${nginx_version}/",
+      provider => shell,
+      command  => "./configure --prefix=/usr/share/nginx --sbin-path=/usr/sbin/nginx --modules-path=/usr/lib64/nginx/modules --conf-path=/etc/nginx/nginx.conf --error-log-path=/var/log/nginx/error.log --http-log-path=/var/log/nginx/access.log --http-client-body-temp-path=/var/lib/nginx/tmp/client_body --http-proxy-temp-path=/var/lib/nginx/tmp/proxy --http-fastcgi-temp-path=/var/lib/nginx/tmp/fastcgi --http-uwsgi-temp-path=/var/lib/nginx/tmp/uwsgi --http-scgi-temp-path=/var/lib/nginx/tmp/scgi --pid-path=/run/nginx.pid --lock-path=/run/lock/subsys/nginx --user=nginx --group=nginx --with-file-aio --with-http_ssl_module --with-http_v2_module --with-http_realip_module --with-stream_ssl_preread_module --with-http_addition_module --with-http_sub_module --with-http_dav_module --with-http_flv_module --with-http_mp4_module --with-http_gunzip_module --with-http_gzip_static_module --with-http_random_index_module --with-http_secure_link_module --with-http_degradation_module --with-http_slice_module --with-http_stub_status_module --with-http_auth_request_module --with-mail=dynamic --with-mail_ssl_module --with-pcre --with-pcre-jit --with-stream=dynamic --with-stream_realip_module --with-stream_ssl_module --add-module=/usr/src/nginx-${nginx_version}/nginx-auth-ldap; make; make install",
+      timeout  => 900,
     }
   }
 
