f305613771ffe2991555c562165cbf1d3b7e8921
anonss <mysticguy9@yahoo.com>
2023-08-15 12:48:53 -0700

    ddf

--- site/profile/manifests/yourls.pp
+++ site/profile/manifests/yourls.pp
@@ -41,6 +41,12 @@ $yourls_db_name = lookup('yourls_db_name')
       command  => "./configure --prefix=/usr/share/nginx --sbin-path=/usr/sbin/nginx --modules-path=/usr/lib64/nginx/modules --conf-path=/etc/nginx/nginx.conf --error-log-path=/var/log/nginx/error.log --http-log-path=/var/log/nginx/access.log --http-client-body-temp-path=/var/lib/nginx/tmp/client_body --http-proxy-temp-path=/var/lib/nginx/tmp/proxy --http-fastcgi-temp-path=/var/lib/nginx/tmp/fastcgi --http-uwsgi-temp-path=/var/lib/nginx/tmp/uwsgi --http-scgi-temp-path=/var/lib/nginx/tmp/scgi --pid-path=/run/nginx.pid --lock-path=/run/lock/subsys/nginx --user=nginx --group=nginx --with-file-aio --with-http_ssl_module --with-http_v2_module --with-http_realip_module --with-stream_ssl_preread_module --with-http_addition_module --with-http_sub_module --with-http_dav_module --with-http_flv_module --with-http_mp4_module --with-http_gunzip_module --with-http_gzip_static_module --with-http_random_index_module --with-http_secure_link_module --with-http_degradation_module --with-http_slice_module --with-http_stub_status_module --with-http_auth_request_module --with-mail=dynamic --with-mail_ssl_module --with-pcre --with-pcre-jit --with-stream=dynamic --with-stream_realip_module --with-stream_ssl_module --add-module=/usr/src/nginx-${nginx_version}/nginx-auth-ldap; /usr/bin/make install",
       timeout  => 900,
     }
+    exec {'make':
+      path     => [ '/usr/bin', '/bin', '/usr/sbin' ],
+      cwd      => "/usr/src/nginx-${nginx_version}",
+      command  => 'make install',
+      timeout  => 900,
+    }
     vcsrepo { "/etc/nginx/YOURLS-${yourls_version}":
       ensure   => present,
       provider => git,
