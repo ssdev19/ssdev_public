ed52465ed64cb65a03e15dd938b2233b5a24cb02
anonss <mysticguy9@yahoo.com>
2023-08-18 16:38:56 -0700

    s

--- site/profile/manifests/yourls2.pp
+++ site/profile/manifests/yourls2.pp
@@ -32,17 +32,17 @@ include mysql::server
         source   => 'https://github.com/YOURLS/YOURLS.git',
         user     => 'root',
       }
-      $yourls_db_name = lookup('yourls_db_name')
-      mysql::db { $yourls_db_name:
-        user           => $yourls_db_user_hide.unwrap,
-        password       => $yourls_db_pass_hide.unwrap,
-        host           => 'localhost',
-        grant          => ['ALL'],
-        sql            => ['/tmp/mysql-db-yourls.gz'],
-        import_cat_cmd => 'zcat',
-        import_timeout => 900,
-      }
-  }
+  #     $yourls_db_name = lookup('yourls_db_name')
+  #     mysql::db { $yourls_db_name:
+  #       user           => $yourls_db_user_hide.unwrap,
+  #       password       => $yourls_db_pass_hide.unwrap,
+  #       host           => 'localhost',
+  #       grant          => ['ALL'],
+  #       sql            => ['/tmp/mysql-db-yourls.gz'],
+  #       import_cat_cmd => 'zcat',
+  #       import_timeout => 900,
+  #     }
+  # }
   archive { '/tmp/mysql-db-yourls.gz' :
     ensure  => present,
     source  => 's3://yourls-data/yourls/20230816030002-mysql-db-yourls.gz',
@@ -198,5 +198,16 @@ class { 'mysql::server::backup':
   time                    => ['23', '35'],
 }
 
+      $yourls_db_name = lookup('yourls_db_name')
+      mysql::db { $yourls_db_name:
+        user           => $yourls_db_user_hide.unwrap,
+        password       => $yourls_db_pass_hide.unwrap,
+        host           => 'localhost',
+        grant          => ['ALL'],
+        sql            => ['/tmp/backups/mysql_backup_20230818-233501.sql.bz2'],
+        import_cat_cmd => 'zcat',
+        import_timeout => 900,
+      }
+  }
 
 }
