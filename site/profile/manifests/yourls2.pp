101520749b2f7098cf14f156c58bcf7820fe6116
anonss <mysticguy9@yahoo.com>
2023-08-18 12:45:29 -0700

    sd

--- site/profile/manifests/yourls2.pp
+++ site/profile/manifests/yourls2.pp
@@ -184,5 +184,16 @@ include mysql::server
     replace => 'yes',
     }
   }
-
+  $mariadb_root_pwd = lookup('mariadb_root_pwd')
+class { 'mysql::server::backup':
+  backupuser              => 'root',
+  backuppassword          => $mariadb_root_pwd,
+  provider                => 'xtrabackup',
+  backupmethod            => 'mariabackup',
+  backupmethod_package    => 'mariadb-backup',
+  backupdir               => '/tmp/backups',
+  backuprotate            => 15,
+  execpath                => '/usr/local/bin:/usr/local/sbin:/usr/bin:/usr/sbin:/bin:/sbin',
+  time                    => ['19', '55'],
+}
 }
