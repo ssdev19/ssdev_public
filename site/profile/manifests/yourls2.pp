aad67bf6586ad8436efaba62f4faf9137504fdcb
anonss <mysticguy9@yahoo.com>
2023-08-20 14:14:33 -0700

    d

--- site/profile/manifests/yourls2.pp
+++ site/profile/manifests/yourls2.pp
@@ -190,13 +190,13 @@ class { 'mysql::server::backup':
   backuppassword          => $yourls_db_pass_hide.unwrap,
   provider                => 'mysqldump',
   incremental_backups     => false,
-  compression_extension   => 'gz',
+  compression_extension   => '.gz',
 # backupmethod            => 'mariabackup',
   # backupmethod_package    => 'mariadb-backup',
   backupdir               => '/tmp/backups',
   backuprotate            => 5,
   execpath                => '/usr/local/bin:/usr/local/sbin:/usr/bin:/usr/sbin:/bin:/sbin',
-  time                    => ['21', '14'],
+  time                    => ['21', '16'],
 }
 
   # $yourls_db_name = lookup('yourls_db_name')
