2e0a72a5ed6f4ba659adf91c80f35aa1aa9781a2
anonss <mysticguy9@yahoo.com>
2023-08-20 13:48:34 -0700

    df

--- site/profile/manifests/yourls2.pp
+++ site/profile/manifests/yourls2.pp
@@ -195,17 +195,17 @@ class { 'mysql::server::backup':
   backupdir               => '/tmp/backups',
   backuprotate            => 5,
   execpath                => '/usr/local/bin:/usr/local/sbin:/usr/bin:/usr/sbin:/bin:/sbin',
-  time                    => ['20', '36'],
+  time                    => ['20', '54'],
 }
 
-  $yourls_db_name = lookup('yourls_db_name')
-  mysql::db { $yourls_db_name:
-    user           => $yourls_db_user_hide.unwrap,
-    password       => $yourls_db_pass_hide.unwrap,
-    host           => 'localhost',
-    grant          => ['ALL'],
-    sql            => ['/tmp/backups/mysql_backup_20230820-203601.sql.bz2'],
-    # import_cat_cmd => 'zcat',
-    import_timeout => 900,
-  }
+  # $yourls_db_name = lookup('yourls_db_name')
+  # mysql::db { $yourls_db_name:
+  #   user           => $yourls_db_user_hide.unwrap,
+  #   password       => $yourls_db_pass_hide.unwrap,
+  #   host           => 'localhost',
+  #   grant          => ['ALL'],
+  #   sql            => ['/tmp/backups/mysql_backup_20230820-203601.sql.bz2'],
+  #   # import_cat_cmd => 'zcat',
+  #   import_timeout => 900,
+  # }
 }
