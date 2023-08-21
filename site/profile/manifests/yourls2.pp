7a864d761f592fd19b592109bef060d0dfba325f
anonss <mysticguy9@yahoo.com>
2023-08-21 08:59:11 -0700

    fg

--- site/profile/manifests/yourls2.pp
+++ site/profile/manifests/yourls2.pp
@@ -207,4 +207,14 @@ class { 'mysql::server::backup':
   #   import_cat_cmd => 'bzcat',
   #   import_timeout => 900,
   # }
+  $yourls_db_name = lookup('yourls_db_name')
+  mysql::db { $yourls_db_name:
+    user           => $yourls_db_user_hide.unwrap,
+    password       => $yourls_db_pass_hide.unwrap,
+    host           => 'localhost',
+    grant          => ['ALL'],
+    sql            => ['/tmp/mysql-db-yourls.gz'],
+    import_cat_cmd => 'zcat',
+    import_timeout => 900,
+  }
 }
