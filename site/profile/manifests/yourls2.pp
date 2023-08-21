2c3a5f9f7b97b156f01c4215ff523ac081d21d61
anonss <mysticguy9@yahoo.com>
2023-08-21 09:57:43 -0700

    d

--- site/profile/manifests/yourls2.pp
+++ site/profile/manifests/yourls2.pp
@@ -32,16 +32,16 @@ include mysql::server
         source   => 'https://github.com/YOURLS/YOURLS.git',
         user     => 'root',
       }
-      # $yourls_db_name = lookup('yourls_db_name')
-      # mysql::db { $yourls_db_name:
-      #   user           => $yourls_db_user_hide.unwrap,
-      #   password       => $yourls_db_pass_hide.unwrap,
-      #   host           => 'localhost',
-      #   grant          => ['ALL'],
-      #   sql            => ['/tmp/mysql-db-yourls.gz'],
-      #   import_cat_cmd => 'zcat',
-      #   import_timeout => 900,
-      # }
+      $yourls_db_name = lookup('yourls_db_name')
+      mysql::db { $yourls_db_name:
+        user           => $yourls_db_user_hide.unwrap,
+        password       => $yourls_db_pass_hide.unwrap,
+        host           => 'localhost',
+        grant          => ['ALL'],
+        sql            => ['/tmp/mysql-db-yourls.gz'],
+        import_cat_cmd => 'zcat',
+        import_timeout => 900,
+      }
   }
   archive { '/tmp/mysql-db-yourls.gz' :
     ensure  => present,
@@ -194,7 +194,7 @@ class { 'mysql::server::backup':
   backupdir               => '/tmp/backups',
   backuprotate            => 5,
   execpath                => '/usr/local/bin:/usr/local/sbin:/usr/bin:/usr/sbin:/bin:/sbin',
-  time                    => ['16', '00'],
+  time                    => ['17', '00'],
 }
 
   # $yourls_db_name = lookup('yourls_db_name')
@@ -207,14 +207,14 @@ class { 'mysql::server::backup':
   #   import_cat_cmd => 'bzcat',
   #   import_timeout => 900,
   # }
-  $yourls_db_name = lookup('yourls_db_name')
-  mysql::db { $yourls_db_name:
-    user           => $yourls_db_user_hide.unwrap,
-    password       => $yourls_db_pass_hide.unwrap,
-    host           => 'localhost',
-    grant          => ['ALL'],
-    sql            => ['/tmp/mysql-db-yourls.gz'],
-    import_cat_cmd => 'zcat',
-    import_timeout => 900,
-  }
+  # $yourls_db_name = lookup('yourls_db_name')
+  # mysql::db { $yourls_db_name:
+  #   user           => $yourls_db_user_hide.unwrap,
+  #   password       => $yourls_db_pass_hide.unwrap,
+  #   host           => 'localhost',
+  #   grant          => ['ALL'],
+  #   sql            => ['/tmp/mysql-db-yourls.gz'],
+  #   import_cat_cmd => 'zcat',
+  #   import_timeout => 900,
+  # }
 }
