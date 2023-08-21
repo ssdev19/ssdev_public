641650197ca0edc75b21a96d9c4e78262b5082b9
anonss <mysticguy9@yahoo.com>
2023-08-21 09:20:12 -0700

    d

--- site/profile/manifests/yourls2.pp
+++ site/profile/manifests/yourls2.pp
@@ -207,7 +207,7 @@ class { 'mysql::server::backup':
   #   import_cat_cmd => 'bzcat',
   #   import_timeout => 900,
   # }
-  $yourls_db_name = lookup('yourls_db_name')
+  # $yourls_db_name = lookup('yourls_db_name')
   mysql::db { $yourls_db_name:
     user           => $yourls_db_user_hide.unwrap,
     password       => $yourls_db_pass_hide.unwrap,
