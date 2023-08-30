89377988f4b17212348e5bfb1795150cbd7940b8
anonss <mysticguy9@yahoo.com>
2023-08-30 10:01:32 -0700

    d

--- site/profile/manifests/ots.pp
+++ site/profile/manifests/ots.pp
@@ -2,7 +2,7 @@
 class profile::ots {
 
 $secret = lookup('secret')
-$redis_pwd = lookup('seredis_pwdcret')
+$redis_pwd = lookup('redis_pwd')
     class { 'onetimesecret':
       version        => 'v0.11.0',
       secret         => $secret,
