179ff28f4b30a657cec44adcb5cf11461c7de4f9
anonss <mysticguy9@yahoo.com>
2023-08-30 18:26:22 -0700

    df

--- site/profile/manifests/ots.pp
+++ site/profile/manifests/ots.pp
@@ -5,7 +5,7 @@ include ::scl
 $secret = lookup('secret')
 $redis_pwd = lookup('redis_pwd')
     class { 'onetimesecret':
-      version        => 'v0.10.1',
+      version        => 'v0.9.2',
       install_dir    => '/opt',
       secret         => $secret,
       redis_password => $redis_pwd,
