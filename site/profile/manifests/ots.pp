a0fb4e50dacb60e6581f167a11e757918fb33964
anonss <mysticguy9@yahoo.com>
2023-09-01 12:11:54 -0700

    fg

--- site/profile/manifests/ots.pp
+++ site/profile/manifests/ots.pp
@@ -5,11 +5,14 @@ include ::scl
 $secret = lookup('secret')
 $redis_pwd = lookup('redis_pwd')
     class { 'onetimesecret':
-      version        => 'v0.11.0',
+      version        => 'e858f1edde6cc6af7ef75aa45f2bb9f9b0f0ecf5', # e858f1edde6cc6af7ef75aa45f2bb9f9b0f0ecf5
       install_dir    => '/opt',
       symlink_name   => '/opt/onetimesecret',
       secret         => $secret,
       redis_password => $redis_pwd,
+      redis_options  => {
+        maxmemory => '2gb',
+      },
     }
 
 # ::scl::collection { 'Powertools':
