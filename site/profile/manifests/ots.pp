4017149452b25dffb1abb412a18f2b7f3cb46109
anonss <mysticguy9@yahoo.com>
2023-09-01 13:36:32 -0700

    df

--- site/profile/manifests/ots.pp
+++ site/profile/manifests/ots.pp
@@ -10,16 +10,16 @@ include 'yum'
 # }
 $secret = lookup('secret')
 $redis_pwd = lookup('redis_pwd')
-    # class { 'onetimesecret':
-    #   version        => 'e858f1edde6cc6af7ef75aa45f2bb9f9b0f0ecf5', # e858f1edde6cc6af7ef75aa45f2bb9f9b0f0ecf5
-    #   install_dir    => '/opt',
-    #   symlink_name   => '/opt/onetimesecret',
-    #   secret         => $secret,
-    #   redis_password => $redis_pwd,
-    #   redis_options  => {
-    #     maxmemory => '2gb',
-    #   },
-    # }
+    class { 'onetimesecret':
+      version        => 'e858f1edde6cc6af7ef75aa45f2bb9f9b0f0ecf5', # e858f1edde6cc6af7ef75aa45f2bb9f9b0f0ecf5
+      install_dir    => '/opt',
+      symlink_name   => '/opt/onetimesecret',
+      secret         => $secret,
+      redis_password => $redis_pwd,
+      redis_options  => {
+        maxmemory => '2gb',
+      },
+    }
 
 # ::scl::collection { 'Powertools':
 #   enable => true,
