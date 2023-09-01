36267af7cf81411760d0985cd999a544f53a4db8
anonss <mysticguy9@yahoo.com>
2023-09-01 13:50:37 -0700

    df

--- site/profile/manifests/ots.pp
+++ site/profile/manifests/ots.pp
@@ -11,7 +11,7 @@ include 'yum'
 $secret = lookup('secret')
 $redis_pwd = lookup('redis_pwd')
     class { 'onetimesecret':
-      version        => 'e858f1edde6cc6af7ef75aa45f2bb9f9b0f0ecf5', # e858f1edde6cc6af7ef75aa45f2bb9f9b0f0ecf5
+      version        => 'e1156b1f8ab98322a898ee4defd1c3f0adb9b5d3', # e858f1edde6cc6af7ef75aa45f2bb9f9b0f0ecf5
       install_dir    => '/opt',
       symlink_name   => '/opt/onetimesecret',
       secret         => $secret,
