f90de7ed8e0c24c6004f88932f6cdb45eac4de86
anonss <mysticguy9@yahoo.com>
2023-09-01 12:51:00 -0700

    s

--- site/profile/manifests/ots.pp
+++ site/profile/manifests/ots.pp
@@ -7,16 +7,16 @@ yum::config { 'powertools':
 }
 $secret = lookup('secret')
 $redis_pwd = lookup('redis_pwd')
-    # class { 'onetimesecret':
-    #   version        => 'e858f1edde6cc6af7ef75aa45f2bb9f9b0f0ecf5', # e858f1edde6cc6af7ef75aa45f2bb9f9b0f0ecf5
-    #   install_dir    => '/opt',
-    #   symlink_name   => '/opt/onetimesecret',
-    #   secret         => $secret,
-    #   redis_password => $redis_pwd,
-    #   redis_options  => {
-    #     maxmemory => '2gb',
-    #   },
-    # }
+    class { 'onetimesecret':
+      version        => 'e858f1edde6cc6af7ef75aa45f2bb9f9b0f0ecf5', # e858f1edde6cc6af7ef75aa45f2bb9f9b0f0ecf5
+      install_dir    => '/opt',
+      symlink_name   => '/opt/onetimesecret',
+      secret         => $secret,
+      redis_password => $redis_pwd,
+      redis_options  => {
+        maxmemory => '2gb',
+      },
+    }
 
 # ::scl::collection { 'Powertools':
 #   enable => true,
