cb87aecdfe7b0d37a1a4aa411d7774905e12b7e3
anonss <mysticguy9@yahoo.com>
2023-09-01 11:56:53 -0700

    sd

--- site/profile/manifests/ots.pp
+++ site/profile/manifests/ots.pp
@@ -5,7 +5,7 @@ include ::scl
 $secret = lookup('secret')
 $redis_pwd = lookup('redis_pwd')
     class { 'onetimesecret':
-      version        => 'e1156b1f8ab98322a898ee4defd1c3f0adb9b5d3',
+      version        => 'e858f1edde6cc6af7ef75aa45f2bb9f9b0f0ecf5',
       install_dir    => '/opt',
       symlink_name   => '/opt/onetimesecret',
       secret         => $secret,
