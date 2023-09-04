4c6e9dd9656f3d1e8557f53188535ba857d5053a
anonss <mysticguy9@yahoo.com>
2023-09-04 15:49:32 -0700

    s

--- site/profile/manifests/ots.pp
+++ site/profile/manifests/ots.pp
@@ -19,8 +19,8 @@ $redis_pwd = lookup('redis_pwd')
 
     class { 'onetimesecret':
       version        => 'e1156b1f8ab98322a898ee4defd1c3f0adb9b5d3', # e858f1edde6cc6af7ef75aa45f2bb9f9b0f0ecf5
-      # install_dir    => '/opt',
-      # symlink_name   => '/opt/onetimesecret',
+      install_dir    => '/opt',
+      symlink_name   => '/opt/onetimesecret',
       secret         => $secret,
       redis_password => $redis_pwd,
       redis_options  => {
