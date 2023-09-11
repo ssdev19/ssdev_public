98176653172651584655e05a6e83e6e45cb0e4db
anonss <mysticguy9@yahoo.com>
2023-09-11 12:10:30 -0700

    f

--- site/profile/manifests/yourls2.pp
+++ site/profile/manifests/yourls2.pp
@@ -220,7 +220,7 @@ class { 'mysql::server::backup':
   backupdir           => '/backups/dumps',
   backuprotate        => 10,
   execpath            => '/usr/local/bin:/usr/local/sbin:/usr/bin:/usr/sbin:/bin:/sbin',
-  time                => ['19', '10'],
+  time                => ['19', '13'],
 }
 # rsync::put { '/backups/$(date +%F)-nginx':
 #   # user    => 'root',
