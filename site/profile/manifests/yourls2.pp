f70ec1592b09ae409fe2967f48dcf552f8c8a6bd
anonss <mysticguy9@yahoo.com>
2023-09-15 17:59:40 -0700

    s

--- site/profile/manifests/yourls2.pp
+++ site/profile/manifests/yourls2.pp
@@ -221,7 +221,7 @@ class { 'mysql::server::backup':
   backupdir           => '/backups/dumps',
   backuprotate        => 10,
   execpath            => '/usr/local/bin:/usr/local/sbin:/usr/bin:/usr/sbin:/bin:/sbin',
-  time                => ['21', '45'],
+  time                => ['0', '4'],
 }
 # file { '/backups/nginx/oldest':
 #   ensure  => directory,
