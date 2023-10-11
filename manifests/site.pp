5f9883c819c20cf20461169b0e175425a96983da
anonss <mysticguy9@yahoo.com>
2023-10-11 13:33:50 -0700

    df

--- manifests/site.pp
+++ manifests/site.pp
@@ -1,11 +1,11 @@
 lookup('classes').include
 
-$packages = lookup(
-  name          => 'packages',
-  value_type    => Variant[Array[String], Undef],
-  merge         => 'unique',
-  default_value => undef,
-)
-if ($packages) {
-  ensure_packages($packages)
-}
+# $packages = lookup(
+#   name          => 'packages',
+#   value_type    => Variant[Array[String], Undef],
+#   merge         => 'unique',
+#   default_value => undef,
+# )
+# if ($packages) {
+#   ensure_packages($packages)
+# }
