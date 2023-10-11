b4f6b9479f399429baf2baef18c0ca4a75572aa5
anonss <mysticguy9@yahoo.com>
2023-10-11 13:27:08 -0700

    s

--- manifests/site.pp
+++ manifests/site.pp
@@ -1,1 +1,8 @@
 lookup('classes').include
+
+$packages = lookup(
+  name          => 'packages',
+  value_type    => Variant[Array[String], Undef],
+  merge         => 'unique',
+  default_value => undef,
+)
