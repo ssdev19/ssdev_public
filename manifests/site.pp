d4bf024ba14ada1366ceb2c4c0ccee59f0660bc5
anonss <mysticguy9@yahoo.com>
2023-10-18 11:24:37 -0700

    s

--- manifests/site.pp
+++ manifests/site.pp
@@ -1,11 +1,13 @@
 lookup('classes').include
 
-$packages = lookup(
-  name          => 'packages',
-  value_type    => Variant[Array[String], Undef],
-  merge         => 'unique',
-  default_value => undef,
-)
-if ($packages) {
-  ensure_packages($packages)
+if $OperatingSystem != 'Linux' {
+  $packages = lookup(
+    name          => 'packages',
+    value_type    => Variant[Array[String], Undef],
+    merge         => 'unique',
+    default_value => undef,
+  )
+  if ($packages) {
+    ensure_packages($packages)
+  }
 }
