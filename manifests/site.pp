fe55f6b68790483babd84e854c9be66e00ee8155
anonss <mysticguy9@yahoo.com>
2023-10-19 12:22:41 -0700

    dc

--- manifests/site.pp
+++ manifests/site.pp
@@ -3,9 +3,9 @@ lookup('classes').include
 if $facts['os']['family'] == 'RedHat' {
   $packages = lookup(
     name          => 'packages',
-    value_type    => Variant[Array[String], Undef],
-    merge         => 'unique',
-    default_value => undef,
+    # value_type    => Variant[Array[String], Undef],
+    # merge         => 'unique',
+    # default_value => undef,
   )
   if ($packages) {
     ensure_packages($packages)
