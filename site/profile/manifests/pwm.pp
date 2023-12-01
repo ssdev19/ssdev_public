591890726c3581eb5eb01fe1575c7c46ac4617ce
anonss <mysticguy9@yahoo.com>
2023-12-01 09:48:19 -0700

    s

--- site/profile/manifests/pwm.pp
+++ site/profile/manifests/pwm.pp
@@ -17,12 +17,12 @@ class profile::pwm {
   # using archive directly to destination breaks tomcat installation
   # So it must first go to the tmp folder then compied over to destination.
 
-  # $dc2cert = lookup('dc2cert')
-  # archive { '/tmp/DC2Cert.cer' :
-  #   ensure  => present,
-  #   source  => $dc2cert,
-  #   cleanup => false,
-  # }
+  $dc2cert = lookup('dc2cert')
+  archive { '/tmp/DC2Cert.cer' :
+    ensure  => present,
+    source  => $dc2cert,
+    cleanup => false,
+  }
   $dc3cert = lookup('dc3cert')
   archive { '/tmp/DC3Cert.cer' :
     ensure  => present,
@@ -108,14 +108,14 @@ class profile::pwm {
   }
 
   # # Manage certs
-  java_ks { 'dc2.lsst.local:/usr/java/jdk-11.0.2+9-jre/lib/security/cacerts':
-    ensure       => latest,
-    certificate  => '/tmp/DC2Cert.cer',
-    # target       => '/usr/java/jdk-11.0.2+9-jre/lib/security/cacerts',
-    password     => $keystorepwd, # Must be at least 6 characters
-    trustcacerts => true,
-    # password_fail_reset => true,
-  }
+  # java_ks { 'dc2.lsst.local:/usr/java/jdk-11.0.2+9-jre/lib/security/cacerts':
+  #   ensure       => latest,
+  #   certificate  => '/tmp/DC2Cert.cer',
+  #   # target       => '/usr/java/jdk-11.0.2+9-jre/lib/security/cacerts',
+  #   password     => $keystorepwd, # Must be at least 6 characters
+  #   trustcacerts => true,
+  #   # password_fail_reset => true,
+  # }
   java_ks { 'dc3.lsst.local:/usr/java/jdk-11.0.2+9-jre/lib/security/cacerts':
     ensure       => latest,
     certificate  => '/tmp/DC3Cert.cer',
