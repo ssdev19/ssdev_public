58ccf79d05ef795646610e60cdda26fe45fee3a9
anonss <mysticguy9@yahoo.com>
2024-01-08 12:50:16 -0700

    d

--- manifests/site.pp
+++ manifests/site.pp
@@ -1,13 +1,13 @@
 lookup('classes').include
 
-if $facts['os']['family'] == 'RedHat' {
-  $packages = lookup(
-    name          => 'packages',
-    # value_type    => Variant[Array[String], Undef],
-    # merge         => 'unique',
-    # default_value => undef,
-  )
-  if ($packages) {
-    ensure_packages($packages)
-  }
+# if $facts['os']['family'] == 'RedHat' {
+#   $packages = lookup(
+#     name          => 'packages',
+#     # value_type    => Variant[Array[String], Undef],
+#     # merge         => 'unique',
+#     # default_value => undef,
+#   )
+#   if ($packages) {
+#     ensure_packages($packages)
+#   }
 }
