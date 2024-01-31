94ac34e8b1bc4aa711e5b3fa864d9538b5072379
anonss <mysticguy9@yahoo.com>
2024-01-31 15:08:54 -0700

    a

--- site/profile/manifests/puppet_master3.pp
+++ site/profile/manifests/puppet_master3.pp
@@ -17,7 +17,7 @@ class profile::puppet_master3 {
   package { 'mygem':
     ensure          => '3.1.6',
     install_options => ['--clear-sources', '--no-document'],
-    provider        => puppetserver_gem,
+    provider        => 'puppetserver_gem',
     source          => "https://rubygems.org/rubygems/rubygems-3.1.6.tgz",
   }
   package { 'toml-rb':
