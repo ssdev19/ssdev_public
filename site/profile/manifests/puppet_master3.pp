7191250494a2676968bf284f173c617fd4a52383
anonss <mysticguy9@yahoo.com>
2024-02-02 10:04:51 -0700

    s

--- site/profile/manifests/puppet_master3.pp
+++ site/profile/manifests/puppet_master3.pp
@@ -10,7 +10,7 @@ class profile::puppet_master3 {
   # include foreman::plugin::templates
   # include foreman_proxy
   # include foreman_proxy::plugin::remote_execution::script
-  # include puppet
+  include puppet
   yumrepo { 'pc_repo':
     ensure   => 'present',
     baseurl  => "http://yum.puppet.com/puppet7/el/${fact('os.release.major')}/x86_64",
@@ -18,7 +18,7 @@ class profile::puppet_master3 {
     enabled  => true,
     gpgcheck => '1',
     gpgkey   => "file:///etc/pki/rpm-gpg/RPM-GPG-KEY-puppet\n  file:///etc/pki/rpm-gpg/RPM-GPG-KEY-puppet-20250406",
-    # before   => Class['puppet'],
+    before   => Class['puppet'],
   }
   # file { '/etc/puppetlabs/puppet/eyaml' :
   #   ensure  => directory,
