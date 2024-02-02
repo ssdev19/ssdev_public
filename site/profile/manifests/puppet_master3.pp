0ffce5825f1924526a9a42937ffe5051293151b0
anonss <mysticguy9@yahoo.com>
2024-02-02 10:48:28 -0700

    d

--- site/profile/manifests/puppet_master3.pp
+++ site/profile/manifests/puppet_master3.pp
@@ -1,15 +1,15 @@
 # puppet master config
 class profile::puppet_master3 {
-  include r10k
-  # include foreman
-  # include foreman::repo
-  # include foreman::plugin::puppet
-  # include foreman::plugin::remote_execution
-  # include foreman_proxy::plugin::remote_execution::script
-  # include foreman::plugin::tasks
-  # include foreman::plugin::templates
-  # include foreman_proxy
-  # include foreman_proxy::plugin::remote_execution::script
+  # include r10k
+  include foreman
+  include foreman::repo
+  include foreman::plugin::puppet
+  include foreman::plugin::remote_execution
+  include foreman_proxy::plugin::remote_execution::script
+  include foreman::plugin::tasks
+  include foreman::plugin::templates
+  include foreman_proxy
+  include foreman_proxy::plugin::remote_execution::script
   include puppet
   yumrepo { 'pc_repo':
     ensure   => 'present',
