ff63235aa34288cd75907af077c08667effdbf3a
anonss <belldex.inc@gmail.com>
2024-03-08 08:02:57 -0700

    d

--- site/profile/manifests/prometheus.pp
+++ site/profile/manifests/prometheus.pp
@@ -52,7 +52,7 @@ class profile::prometheus (
   class { 'prometheus::alertmanager':
     # extra_options => '--cluster.listen-address=',
     # extra_options => "--cluster.advertise-address=${advertise_ip} \--cluster.listen-address=:9797 \--cluster.peer=${unwrap($cluster_hide)}",
-    version       => '0.26.0',
+    version       => '0.27.0',
     # global    => {
     #   'resolve_timeout' => '1m',
     #   'to'              => 'wf@belldex.com',
