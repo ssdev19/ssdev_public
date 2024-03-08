a005010de5d94aa9431fa9d05e1810c2847e76be
anonss <belldex.inc@gmail.com>
2024-03-08 12:42:22 -0700

    d

--- site/profile/lib/facter/hardware_platform.rb
+++ site/profile/lib/facter/hardware_platform.rb
@@ -1,10 +1,10 @@
 # hardware_platform.rb
 
-Facter.add('hardware_platform') do
-    setcode do
-      Facter::Core::Execution.execute('/bin/uname --hardware-platform')
-    end
-  end
+# Facter.add('hardware_platform') do
+#     setcode do
+#       Facter::Core::Execution.execute('/bin/uname --hardware-platform')
+#     end
+#   end
   
 # Facter.add(:osfamily) do
 #   setcode do
