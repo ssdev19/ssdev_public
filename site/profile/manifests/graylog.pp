41197d3382077ad9c910bf5ab99e951dc8e557bf
anonss <belldex.inc@gmail.com>
2024-03-16 11:40:44 -0700

    d

--- site/profile/manifests/graylog.pp
+++ site/profile/manifests/graylog.pp
@@ -59,7 +59,7 @@ class profile::graylog {
       allow_leading_wildcard_searches     => true,
       allow_highlighting                  => true,
       http_bind_address                   => '0.0.0.0:9000',
-      http_external_uri                   => 'https://graylog-ssdev.us.lsst.org:9000/',
+      http_external_uri                   => 'http://graylog-ssdev.us.lsst.org:9000/',
       # http_enable_tls                     => true,
       # http_tls_cert_file                  => '/etc/ssl/graylog/graylog_cert_chain.crt',
       # http_tls_key_file                   => '/etc/ssl/graylog/graylog_key_pkcs8.pem',
