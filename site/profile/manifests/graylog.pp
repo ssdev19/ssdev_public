e93f0c8d2bd507d2bb9aa6835e028e1a074b3884
anonss <belldex.inc@gmail.com>
2024-03-16 12:23:34 -0700

    f

--- site/profile/manifests/graylog.pp
+++ site/profile/manifests/graylog.pp
@@ -81,8 +81,8 @@ class profile::graylog {
       elasticsearch_shards                => '4',
       elasticsearch_replicas              => '1',
       elasticsearch_index_prefix          => 'graylog',
-      elasticsearch_hosts                 => 'https://graylog-ssdev.us.lsst.org:9200',
-      mongodb_uri                         => 'mongodb://graylog-ssdev.us.lsst.org/graylog',
+      # elasticsearch_hosts                 => 'https://graylog-ssdev.us.lsst.org:9200',
+      # mongodb_uri                         => 'mongodb://graylog-ssdev.us.lsst.org/graylog',
     },
   }
 }
