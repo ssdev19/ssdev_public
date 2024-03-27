08e33de0fd54d7ef331685f0195824f08d8f9986
anonss <belldex.inc@gmail.com>
2024-03-27 10:30:41 -0700

    ff

--- site/profile/manifests/graylog.pp
+++ site/profile/manifests/graylog.pp
@@ -107,7 +107,7 @@ class profile::graylog {
   graylog_api { 'api':
     username => 'admin',
     password => $glog_pwd,
-    port     => 443,
+    port     => 9000,
     tls      => false,
     server   => 'graylog-ssdev.us.lsst.org',
   }
