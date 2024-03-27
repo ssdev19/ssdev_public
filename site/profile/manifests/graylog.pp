8b86cb9c01c8a613a2934987e2db53e9cc4695ff
anonss <belldex.inc@gmail.com>
2024-03-27 11:23:57 -0700

    d

--- site/profile/manifests/graylog.pp
+++ site/profile/manifests/graylog.pp
@@ -86,7 +86,7 @@ class profile::graylog {
       root_timezone                       => 'Europe/Berlin',
       allow_leading_wildcard_searches     => false,
       allow_highlighting                  => false,
-      http_bind_address                   => '0.0.0.0:9000',
+      http_bind_address                   => '0.0.0.0:443',
       http_external_uri                   => 'https://graylog-ssdev.lsst.org:9000/',
       http_enable_tls                     => true,
       http_tls_cert_file                  => '/etc/ssl/certs/graylog/graylog.crt',
@@ -107,7 +107,7 @@ class profile::graylog {
   graylog_api { 'api':
     username => 'admin',
     password => $glog_pwd,
-    port     => 9000,
+    port     => 443,
     tls      => true,
     server   => 'graylog-ssdev.lsst.org',
   }
