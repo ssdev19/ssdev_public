946843f0c9d1fb928068eb668eb3f7f357fa7df2
anonss <belldex.inc@gmail.com>
2024-04-06 09:09:02 -0700

    d

--- site/profile/manifests/graylog.pp
+++ site/profile/manifests/graylog.pp
@@ -120,7 +120,7 @@ class profile::graylog {
       http_bind_address                   => '0.0.0.0:9000',
       http_external_uri                   => "https://${fqdn}:9000/",
       http_publish_uri                    => "https://${fqdn}:9000/",
-      http_enable_tls                     => false,
+      http_enable_tls                     => true,
       http_tls_cert_file                  => '/etc/ssl/certs/graylog/graylog.crt',
       http_tls_key_file                   => '/etc/ssl/certs/graylog/graylog.key',
       # http_tls_key_password               => 'pwdtest',
