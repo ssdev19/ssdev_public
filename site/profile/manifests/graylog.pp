82315aab0e08ff36dbabb2b0cfc341e6522a764c
anonss <belldex.inc@gmail.com>
2024-04-06 12:38:58 -0700

    f

--- site/profile/manifests/graylog.pp
+++ site/profile/manifests/graylog.pp
@@ -43,7 +43,8 @@ class profile::graylog {
   }
   ->class { 'mongodb::server':
     bind_ip       => ['127.0.0.1'],
-    set_parameter => ['diagnosticDataCollectionEnabled: false'], # prvents mongodb from scanning directories which was being blocked by SELinux
+    set_parameter => ['diagnosticDataCollectionEnabled: false'], 
+    # above prvents mongodb from scanning directories which was being blocked by SELinux
   }
 
 # Install OpenSearch repository and packages
