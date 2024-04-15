bf832c3d84b15da576cd91ae38cdf547b07da9b8
anonss <belldex.inc@gmail.com>
2024-04-15 12:50:35 -0700

    s

--- site/profile/manifests/letsencrypt.pp
+++ site/profile/manifests/letsencrypt.pp
@@ -16,7 +16,7 @@ class profile::letsencrypt ( Sensitive[String]
   }
   letsencrypt::certonly { $host:
     # ensure      => 'absent',
-    domains     => ['lsst.org'],
+    domains     => ['*.lsst.org'],
     config_dir  => '/etc/ssl/certs/graylog/',
     # manage_cron          => true,
     # cron_hour            => [0,12],
