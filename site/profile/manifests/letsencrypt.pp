04ba87b7d52bc0127deefcffd9bc7868407aa71a
anonss <belldex.inc@gmail.com>
2024-04-15 10:50:14 -0700

    d

--- site/profile/manifests/letsencrypt.pp
+++ site/profile/manifests/letsencrypt.pp
@@ -3,20 +3,22 @@ class profile::letsencrypt {
   $host = $facts['networking']['hostname']
   $fqdn = $facts['networking']['fqdn']
   $email = lookup('email')
-class { 'letsencrypt':
-  config => {
-    email  => $email,
-    server => 'https://acme-v01.api.letsencrypt.org/directory',
+  include epel
+  class { 'letsencrypt':
+    config            => {
+      email  => $email,
+      server => 'https://acme-v01.api.letsencrypt.org/directory',
+    },
+    renew_cron_ensure => 'present',
   }
-}
-letsencrypt::certonly { $host:
-  domains     => [$fqdn],
-  # config_dir  => '/etc/ssl/graylog/',
-  # manage_cron          => true,
-  # cron_hour            => [0,12],
-  # cron_minute          => '30',
-  # cron_before_command  => 'service graylog-server stop',
-  # cron_success_command => '/bin/systemctl reload graylog-server.service',
-  # suppress_cron_output => true,
-}
+  # letsencrypt::certonly { $host:
+  #   domains     => [$fqdn],
+  #   # config_dir  => '/etc/ssl/graylog/',
+  #   # manage_cron          => true,
+  #   # cron_hour            => [0,12],
+  #   # cron_minute          => '30',
+  #   # cron_before_command  => 'service graylog-server stop',
+  #   # cron_success_command => '/bin/systemctl reload graylog-server.service',
+  #   # suppress_cron_output => true,
+  # }
 }
