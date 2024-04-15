9833f2ec63487d22b9e98ea27b2d8df248b35010
anonss <belldex.inc@gmail.com>
2024-04-15 11:18:27 -0700

    f

--- site/profile/manifests/letsencrypt.pp
+++ site/profile/manifests/letsencrypt.pp
@@ -14,14 +14,14 @@ class profile::letsencrypt ( Sensitive[String]
     },
     renew_cron_ensure => 'present',
   }
-  # letsencrypt::certonly { $host:
-  #   domains     => [$fqdn],
-  #   # config_dir  => '/etc/ssl/graylog/',
-  #   # manage_cron          => true,
-  #   # cron_hour            => [0,12],
-  #   # cron_minute          => '30',
-  #   # cron_before_command  => 'service graylog-server stop',
-  #   # cron_success_command => '/bin/systemctl reload graylog-server.service',
-  #   # suppress_cron_output => true,
-  # }
+  letsencrypt::certonly { $host:
+    domains     => [$fqdn],
+    config_dir  => '/etc/ssl/graylog/',
+    # manage_cron          => true,
+    # cron_hour            => [0,12],
+    # cron_minute          => '30',
+    # cron_before_command  => 'service graylog-server stop',
+    # cron_success_command => '/bin/systemctl reload graylog-server.service',
+    # suppress_cron_output => true,
+  }
 }
