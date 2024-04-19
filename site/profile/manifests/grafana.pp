56f2bf612f7a8f531b3bbf543a614673132f7c2a
anonss <belldex.inc@gmail.com>
2024-04-19 09:55:47 -0700

    f

--- site/profile/manifests/grafana.pp
+++ site/profile/manifests/grafana.pp
@@ -58,6 +58,6 @@ class profile::grafana (
     ensure  => present,
     source  => $domaincert2,
     cleanup => false,
-    require => service["grafana"]
+    require => service['grafana-server'],
   }
 }
