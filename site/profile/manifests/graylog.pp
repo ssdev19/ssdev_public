8e62e6814e041d05a766424ba02e32636053c716
anonss <belldex.inc@gmail.com>
2024-04-24 11:49:32 -0700

    d

--- site/profile/manifests/graylog.pp
+++ site/profile/manifests/graylog.pp
@@ -102,9 +102,9 @@ class profile::graylog {
   $keystorepwd = lookup('keystorepwd')
   java_ks { "lsst.org:${ssldir}/cacerts.jks":
     ensure              => latest,
-    certificate         => "${le_dir}/cert.pem",
-    private_key         => "${le_dir}/privkey.pem",
-    chain               => "${le_dir}/fullchain.pem",
+    certificate         => "${le_dir}/fullchain.pem",
+    # private_key         => "${le_dir}/privkey.pem",
+    # chain               => "${le_dir}/fullchain.pem",
     password            => $keystorepwd,
     password_fail_reset => true,
   }
