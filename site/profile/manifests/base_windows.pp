3b8e6798170cad5cd78128adcb845336eed41f0b
anonss <belldex.inc@gmail.com>
2024-05-29 15:30:26 -0700

    df

--- site/profile/manifests/base_windows.pp
+++ site/profile/manifests/base_windows.pp
@@ -33,6 +33,10 @@ class profile::base_windows (
     source => 'http://wsus.lsst.org/puppetfiles/nxlog/nxlog3.2.2329.conf',
     notify => Service['nxlog'],
   }
+  file { 'C:/Program Files (x86)/nxlog/':
+    ensure => absent,
+  }
+
   service { 'nxlog':
     ensure => 'running',
     enable => true,
