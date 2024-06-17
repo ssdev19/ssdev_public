6e10accda9e814b824707cf97b3e2daefeb55305
anonss <belldex.inc@gmail.com>
2024-06-17 14:31:23 -0700

    q

--- site/profile/manifests/ots.pp
+++ site/profile/manifests/ots.pp
@@ -11,22 +11,26 @@ class profile::ots {
   $secret = lookup('secret')
   $redis_pwd = lookup('redis_pwd')
 
-  # rvm_system_ruby {
-  #   'ruby-2.6':
-  #     ensure      => 'present',
-  #     default_use => true;
-  # }
-
-  class { 'onetimesecret':
-    version        => 'e1156b1f8ab98322a898ee4defd1c3f0adb9b5d3', # e858f1edde6cc6af7ef75aa45f2bb9f9b0f0ecf5
-    install_dir    => '/opt',
-    symlink_name   => '/opt/onetimesecret',
-    secret         => $secret,
-    redis_password => $redis_pwd,
-    redis_options  => {
-      maxmemory => '2gb',
-    },
+  rvm_system_ruby {
+    'ruby-3.2':
+      ensure      => 'present',
+      default_use => true,
+      build_opts  => ['--binary'];
+    'ruby-3.1':
+      ensure      => 'present',
+      default_use => false;
   }
+
+  # class { 'onetimesecret':
+  #   version        => 'e1156b1f8ab98322a898ee4defd1c3f0adb9b5d3', # e858f1edde6cc6af7ef75aa45f2bb9f9b0f0ecf5
+  #   install_dir    => '/opt',
+  #   symlink_name   => '/opt/onetimesecret',
+  #   secret         => $secret,
+  #   redis_password => $redis_pwd,
+  #   redis_options  => {
+  #     maxmemory => '2gb',
+  #   },
+  # }
 # ::scl::collection { 'Powertools':
 #   enable => true,
 # }
