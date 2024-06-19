7ede5271b2c2e70a76fe2213b1ba3c1adfc85c7f
anonss <belldex.inc@gmail.com>
2024-06-19 10:34:06 -0700

    s

--- site/profile/manifests/ots.pp
+++ site/profile/manifests/ots.pp
@@ -13,7 +13,7 @@ class profile::ots {
   $redis_pwd = lookup('redis_pwd')
   # class { 'rvm': }
   rvm_system_ruby {
-    'ruby-3.2.4':
+    'ruby-3.1':
       ensure      => 'present',
       default_use => true,
       build_opts  => ['--binary'];
