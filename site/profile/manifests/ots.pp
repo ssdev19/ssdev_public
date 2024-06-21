4c58951a62259f541ef395d8d55b423be92d9b19
anonss <belldex.inc@gmail.com>
2024-06-21 13:03:55 -0700

    f

--- site/profile/manifests/ots.pp
+++ site/profile/manifests/ots.pp
@@ -10,6 +10,7 @@ class profile::ots {
     ipaddress => '140.252.32.192',
     method    => 'static',
     netmask   => '255.255.254.0',
+    gateway   => '140.252.32.2',
     onboot    => 'true',
   }
 # yum::config { 'powertools':
