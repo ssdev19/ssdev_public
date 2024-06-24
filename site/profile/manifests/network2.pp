c0b569454e61a052a8db6e45e0fd1fa52349fc34
anonss <belldex.inc@gmail.com>
2024-06-24 12:34:40 -0700

    d

--- site/profile/manifests/network2.pp
+++ site/profile/manifests/network2.pp
@@ -1,7 +1,7 @@
 
 class profile::network2 ($interfaces = hiera('interfaces',false)) {
   if ($interfaces != false) {
-    include 'network'
+    include '::network'
     create_resources ( network_config, $interfaces )
 
   }
