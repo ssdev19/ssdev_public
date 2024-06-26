51a34eef2b90fea7acf6e272d81d4768f529f2df
anonss <belldex.inc@gmail.com>
2024-06-26 09:58:48 -0700

    d

--- site/profile/manifests/network2.pp
+++ site/profile/manifests/network2.pp
@@ -7,9 +7,11 @@ class profile::network2 { #($interfaces = hiera('interfaces',false)) {
   #   create_resources ( network_config, $interfaces )
   # }
 
-  $rules = hiera('interfaces')
+  # $rules = hiera('interfaces')
   # $defaults = {
   #   netmask => '255.255.252.0',
   # }
-  create_resources('network_config', $rules)
+  # create_resources('network_config', $rules)
+
+  create_resources('network_config', hiera('network_config'))
 }
