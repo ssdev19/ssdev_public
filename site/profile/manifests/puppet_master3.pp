bc1ddcad28a563b050768eb5afe4ec1474e7538d
anonss <belldex.inc@gmail.com>
2024-06-26 17:08:50 -0700

    d

--- site/profile/manifests/puppet_master3.pp
+++ site/profile/manifests/puppet_master3.pp
@@ -18,7 +18,7 @@ class profile::puppet_master3 {
     provider => 'puppetserver_gem',
   }
   package { 'hiera-eyaml':
-    ensure   => latest,
+    ensure   => '4.1.0',
     provider => 'puppetserver_gem',
   }
   # package { 'eyaml':
