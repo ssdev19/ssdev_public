e645bec8be0898c2b59530336c6f5b6be6077d05
anonss <belldex.inc@gmail.com>
2024-07-01 19:00:58 -0700

    d

--- site/profile/manifests/ots.pp
+++ site/profile/manifests/ots.pp
@@ -26,16 +26,16 @@ class profile::ots {
     #   default_use => false;
   }
 
-  # class { 'onetimesecret':
-  #   version        => 'e1156b1f8ab98322a898ee4defd1c3f0adb9b5d3', # e858f1edde6cc6af7ef75aa45f2bb9f9b0f0ecf5
-  #   install_dir    => '/opt',
-  #   symlink_name   => '/opt/onetimesecret',
-  #   secret         => $secret,
-  #   redis_password => $redis_pwd,
-  #   redis_options  => {
-  #     maxmemory => '2gb',
-  #   },
-  # }
+  class { 'onetimesecret':
+    version        => 'e1156b1f8ab98322a898ee4defd1c3f0adb9b5d3', # e858f1edde6cc6af7ef75aa45f2bb9f9b0f0ecf5
+    install_dir    => '/opt',
+    symlink_name   => '/opt/onetimesecret',
+    secret         => $secret,
+    redis_password => $redis_pwd,
+    redis_options  => {
+      maxmemory => '2gb',
+    },
+  }
 # ::scl::collection { 'Powertools':
 #   enable => true,
 # }
