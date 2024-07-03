c26ca4b0720d04c31d5f199ade52f3ab0f7dfeaa
anonss <belldex.inc@gmail.com>
2024-07-03 15:47:58 -0700

    s

--- site/profile/manifests/ots.pp
+++ site/profile/manifests/ots.pp
@@ -26,7 +26,7 @@ class profile::ots {
 # bundle update --bundler
 # Reboot needed after installation
   rvm_system_ruby {
-    'ruby-3.2.1':
+    'ruby-3.2.0':
       ensure      => 'present',
       default_use => true,
       # build_opts  => ['--binary'],
