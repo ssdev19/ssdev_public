e3beabdd2bec46264530ddc2212ccb55dcefe478
anonss <belldex.inc@gmail.com>
2024-07-03 07:40:55 -0700

    f

--- site/profile/manifests/ots.pp
+++ site/profile/manifests/ots.pp
@@ -1,8 +1,8 @@
 # ots
 class profile::ots {
-  include '::gnupg'
-  include ::scl
-  include 'yum'
+  # include '::gnupg'
+  # include ::scl
+  # include 'yum'
   # include redis
   # include rvm
 
@@ -12,19 +12,19 @@ class profile::ots {
 # }
   $secret = lookup('secret')
   $redis_pwd = lookup('redis_pwd')
-  class { 'rvm': }
+  # class { 'rvm': }
 
 # Reboot needed after installation
-  rvm_system_ruby {
-    'ruby-3.1.1':
-      ensure      => 'present',
-      default_use => true,
-      # build_opts  => ['--binary'],
-      ;
-    # 'ruby-3.3.3':
-    #   ensure      => 'present',
-    #   default_use => false;
-  }
+  # rvm_system_ruby {
+  #   'ruby-3.1.1':
+  #     ensure      => 'present',
+  #     default_use => true,
+  #     # build_opts  => ['--binary'],
+  #     ;
+  #   # 'ruby-3.3.3':
+  #   #   ensure      => 'present',
+  #   #   default_use => false;
+  # }
 
   class { 'onetimesecret':
     version        => 'e1156b1f8ab98322a898ee4defd1c3f0adb9b5d3', # e858f1edde6cc6af7ef75aa45f2bb9f9b0f0ecf5
