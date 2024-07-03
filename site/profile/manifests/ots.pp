3dfd0bb789ed1f8282e046ebb6cdd97ff5b910fe
anonss <belldex.inc@gmail.com>
2024-07-03 08:21:10 -0700

    s

--- site/profile/manifests/ots.pp
+++ site/profile/manifests/ots.pp
@@ -1,15 +1,16 @@
 # ots
 class profile::ots {
+  include onetimesecret
   # include '::gnupg'
   # include ::scl
   include 'yum'
   # include redis
   # include rvm
 
-  yum::config { 'powertools':
-    ensure  => present,
-    # enabled => true,
-  }
+  # yum::config { 'powertools':
+  #   ensure  => present,
+  #   # enabled => true,
+  # }
   $secret = lookup('secret')
   $redis_pwd = lookup('redis_pwd')
   # class { 'rvm': }
@@ -26,16 +27,16 @@ class profile::ots {
   #   #   default_use => false;
   # }
 
-  class { 'onetimesecret':
-    version        => 'e1156b1f8ab98322a898ee4defd1c3f0adb9b5d3', # e858f1edde6cc6af7ef75aa45f2bb9f9b0f0ecf5
-    install_dir    => '/opt',
-    symlink_name   => '/opt/onetimesecret',
-    secret         => $secret,
-    redis_password => $redis_pwd,
-    redis_options  => {
-      maxmemory => '2gb',
-    },
-  }
+  # class { 'onetimesecret':
+  #   version        => 'e1156b1f8ab98322a898ee4defd1c3f0adb9b5d3', # e858f1edde6cc6af7ef75aa45f2bb9f9b0f0ecf5
+  #   install_dir    => '/opt',
+  #   symlink_name   => '/opt/onetimesecret',
+  #   secret         => $secret,
+  #   redis_password => $redis_pwd,
+  #   redis_options  => {
+  #     maxmemory => '2gb',
+  #   },
+  # }
 # ::scl::collection { 'Powertools':
 #   enable => true,
 # }
