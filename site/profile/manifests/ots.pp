e9ec812fa0b67a270566db579e411d5834faafed
anonss <belldex.inc@gmail.com>
2024-07-03 15:31:36 -0700

    s

--- site/profile/manifests/ots.pp
+++ site/profile/manifests/ots.pp
@@ -4,7 +4,7 @@ class profile::ots {
   # include '::gnupg'
   # include ::scl
   include 'yum'
-  # include redis
+  include redis
   include rvm
 
   # yum::config { 'powertools':
@@ -14,11 +14,11 @@ class profile::ots {
   $secret = lookup('secret')
   $redis_pwd = lookup('redis_pwd')
   # class { 'rvm': }
-  # file { '/opt/onetimesecret-v0.14.0' :
-  #   ensure  => directory,
-  #   # owner   => 'ots',
-  #   # grubyroup   => 'ots',
-  # }
+  file { '/opt/onetimesecret' :
+    ensure  => directory,
+    # owner   => 'ots',
+    # grubyroup   => 'ots',
+  }
 # Manually install ruby:  rvm install "ruby-2.6.0"
 # Set it to default:  rvm 2.6.0 --default
 # gem update --system 3.2.3
@@ -26,14 +26,14 @@ class profile::ots {
 # bundle update --bundler
 # Reboot needed after installation
   rvm_system_ruby {
-    'ruby-2.7.8':
+    'ruby-3.2.1':
       ensure      => 'present',
       default_use => true,
       # build_opts  => ['--binary'],
       ;
-    'ruby-2.6.0':
-      ensure      => 'present',
-      default_use => false;
+    # 'ruby-2.6.0':
+    #   ensure      => 'present',
+    #   default_use => false;
   }
   # rvm_gem {
   #   'bundler':
@@ -42,21 +42,27 @@ class profile::ots {
   #     ruby_version => 'ruby-2.6.0',
   #     require      => Rvm_system_ruby['ruby-2.6.0'];
   # }
-  class { 'onetimesecret':
-    version        => 'v0.14.0',  #'e858f1edde6cc6af7ef75aa45f2bb9f9b0f0ecf5', #  e1156b1f8ab98322a898ee4defd1c3f0adb9b5d3
-    install_dir    => '/opt',
-    symlink_name   => '/opt/onetimesecret',
-    secret         => $secret,
-    redis_password => $redis_pwd,
-    redis_options  => {
-      maxmemory => '2gb',
-    },
+  vcsrepo { "/opt/onetimesecret":
+    ensure   => present,
+    provider => git,
+    source   => 'https://github.com/onetimesecret/onetimesecret.git',
+    user     => 'root',
   }
-  file { '/opt/onetimesecret-v0.14.0':
-    ensure => link,
-    target => '/opt/onetimesecret-0.14.0',
-    # require => Class['graylog-server'],
-  }
+  # class { 'onetimesecret':
+  #   version        => 'v0.14.0',  #'e858f1edde6cc6af7ef75aa45f2bb9f9b0f0ecf5', #  e1156b1f8ab98322a898ee4defd1c3f0adb9b5d3
+  #   install_dir    => '/opt',
+  #   symlink_name   => '/opt/onetimesecret',
+  #   secret         => $secret,
+  #   redis_password => $redis_pwd,
+  #   redis_options  => {
+  #     maxmemory => '2gb',
+  #   },
+  # }
+  # file { '/opt/onetimesecret-v0.14.0':
+  #   ensure => link,
+  #   target => '/opt/onetimesecret-0.14.0',
+  #   # require => Class['graylog-server'],
+  # }
 # ::scl::collection { 'Powertools':
 #   enable => true,
 # }
