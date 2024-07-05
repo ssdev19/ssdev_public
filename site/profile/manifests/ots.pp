860411c890f45a4e618810ffbdb24da12024ab16
anonss <belldex.inc@gmail.com>
2024-07-05 08:39:46 -0700

    f

--- site/profile/manifests/ots.pp
+++ site/profile/manifests/ots.pp
@@ -35,13 +35,18 @@ class profile::ots {
       ensure      => 'present',
       default_use => false;
   }
-  rvm_gem {
-    'bundler':
-      ensure       => latest,
-      name         => 'bundler',
-      ruby_version => 'ruby-3.2.2',
-      require      => Rvm_system_ruby['ruby-3.2.2'];
+  rvm_gemset {
+    'ruby-3.2.2':
+      ensure  => present,
+      require => Rvm_system_ruby['ruby-3.2.2'];
   }
+  # rvm_gem {
+  #   'bundler':
+  #     ensure       => latest,
+  #     name         => 'bundler',
+  #     ruby_version => 'ruby-3.2.2',
+  #     require      => Rvm_system_ruby['ruby-3.2.2'];
+  # }
   vcsrepo { "/opt/onetimesecret":
     ensure   => present,
     provider => git,
