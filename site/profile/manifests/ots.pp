25547be0ff878cf9384dabc52a74d25db3f5c638
anonss <belldex.inc@gmail.com>
2024-07-05 09:14:07 -0700

    s

--- site/profile/manifests/ots.pp
+++ site/profile/manifests/ots.pp
@@ -4,8 +4,8 @@ class profile::ots {
   # include '::gnupg'
   # include ::scl
   include 'yum'
-  include redis
-  include rvm
+  # include redis
+  # include rvm
 
   # yum::config { 'powertools':
   #   ensure  => present,
@@ -25,21 +25,21 @@ class profile::ots {
 # gem install bundler:2.4.12
 # bundle update --bundler
 # Reboot needed after installation perhaps a few times
-  rvm_system_ruby {
-    'ruby-3.2.2':
-      ensure      => 'present',
-      default_use => true,
-      # build_opts  => ['--binary'],
-      ;
-    'ruby-2.5.9':
-      ensure      => 'present',
-      default_use => false;
-  }
-  rvm_gemset {
-    'ruby-2.5.9@testing':
-      ensure  => present,
-      require => Rvm_system_ruby['ruby-2.5.9'];
-  }
+  # rvm_system_ruby {
+  #   'ruby-3.2.2':
+  #     ensure      => 'present',
+  #     default_use => true,
+  #     # build_opts  => ['--binary'],
+  #     ;
+  #   'ruby-2.5.9':
+  #     ensure      => 'present',
+  #     default_use => false;
+  # }
+  # rvm_gemset {
+  #   'ruby-2.5.9@testing':
+  #     ensure  => present,
+  #     require => Rvm_system_ruby['ruby-2.5.9'];
+  # }
   # rvm_gem {
   #   'bundler':
   #     ensure       => latest,
@@ -47,22 +47,22 @@ class profile::ots {
   #     ruby_version => 'ruby-3.2.2',
   #     require      => Rvm_system_ruby['ruby-3.2.2'];
   # }
-  vcsrepo { "/opt/onetimesecret":
-    ensure   => present,
-    provider => git,
-    source   => 'https://github.com/onetimesecret/onetimesecret.git',
-    user     => 'root',
-  }
-  # class { 'onetimesecret':
-  #   version        => 'v0.14.0',  #'e858f1edde6cc6af7ef75aa45f2bb9f9b0f0ecf5', #  e1156b1f8ab98322a898ee4defd1c3f0adb9b5d3
-  #   install_dir    => '/opt',
-  #   symlink_name   => '/opt/onetimesecret',
-  #   secret         => $secret,
-  #   redis_password => $redis_pwd,
-  #   redis_options  => {
-  #     maxmemory => '2gb',
-  #   },
+  # vcsrepo { "/opt/onetimesecret":
+  #   ensure   => present,
+  #   provider => git,
+  #   source   => 'https://github.com/onetimesecret/onetimesecret.git',
+  #   user     => 'root',
   # }
+  class { 'onetimesecret':
+    version        => 'v0.14.0',  #'e858f1edde6cc6af7ef75aa45f2bb9f9b0f0ecf5', #  e1156b1f8ab98322a898ee4defd1c3f0adb9b5d3
+    install_dir    => '/opt',
+    symlink_name   => '/opt/onetimesecret',
+    secret         => $secret,
+    redis_password => $redis_pwd,
+    redis_options  => {
+      maxmemory => '2gb',
+    },
+  }
   # file { '/opt/onetimesecret-v0.14.0':
   #   ensure => link,
   #   target => '/opt/onetimesecret-0.14.0',
