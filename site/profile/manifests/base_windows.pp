1e2005ebf5e33add2ed4db144b664ca70eb85879
anonss <belldex.inc@gmail.com>
2024-08-09 14:24:02 -0700

    d

--- site/profile/manifests/base_windows.pp
+++ site/profile/manifests/base_windows.pp
@@ -14,7 +14,7 @@ class profile::base_windows (
 
   package { 'windows_exporter':
     ensure => '0.19.0',
-    source => 'https://github.com/prometheus-community/windows_exporter/releases/download/v0.26.2/windows_exporter-0.26.2-amd64.msi',
+    source => 'https://github.com/prometheus-community/windows_exporter/releases/download/v0.19.0/windows_exporter-0.19.0-amd64.msi',
     notify => Service['windows_exporter'],
   }
   package { 'Notepad++ (64-bit x64)':
