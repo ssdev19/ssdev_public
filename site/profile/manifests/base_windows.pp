d01cf20371876c28a583223b7fe57d09e93aa106
anonss <belldex.inc@gmail.com>
2024-08-09 14:19:45 -0700

    f

--- site/profile/manifests/base_windows.pp
+++ site/profile/manifests/base_windows.pp
@@ -13,8 +13,9 @@ class profile::base_windows (
   include chocolatey # Needed for just about most things for Windows.
 
   package { 'windows_exporter':
-    ensure => '0.26.2',
-    source => 'https://github.com/prometheus-community/windows_exporter/releases/download/v0.26.2/windows_exporter-0.26.2-amd64.msi'
+    ensure => '0.24.0',
+    source => 'https://github.com/prometheus-community/windows_exporter/releases/download/v0.26.2/windows_exporter-0.26.2-amd64.msi',
+    notify => Service['windows_exporter'],
   }
   package { 'Notepad++ (64-bit x64)':
     ensure          => installed,
