7c57f142c4cfa644ff3735f7cdcd5728cb77f5df
anonss <belldex.inc@gmail.com>
2024-10-02 16:16:19 -0700

    f

--- site/profile/manifests/base_linux.pp
+++ site/profile/manifests/base_linux.pp
@@ -75,7 +75,7 @@ class profile::base_linux (
   Package {['git', 'tree', 'tcpdump', 'telnet', 'lvm2', 'gcc',
       'bash-completion', 'sudo', 'vim', 'openssl', 'openssl-devel',
       'acpid', 'wget', 'nmap', 'iputils', 'bind-utils', 'traceroute',
-      'gzip', 'tar', 'unzip', 'net-tools', 'fping',
+      'gzip', 'tar', 'unzip', 'net-tools',
     ]:
       ensure => installed,
   }
