3997e2ae163682471690abea0c6c4beb8246918d
anonss <belldex.inc@gmail.com>
2024-11-27 11:23:14 -0700

    b

--- site/profile/manifests/ots.pp
+++ site/profile/manifests/ots.pp
@@ -54,7 +54,7 @@ class profile::ots {
   #   user     => 'root',
   # }
   class { 'onetimesecret':
-    version        => 'v0.18.3',
+    version        => 'v0.9.2',
     secret         => 'SomeHardToGuessRandomCharacters',
     redis_password => 'AnotherGoodPassword',
   }
