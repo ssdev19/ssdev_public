cfd420be53e3e600534911cf1ffdb5ff9e06025d
anonss <belldex.inc@gmail.com>
2024-11-29 12:06:47 -0700

    g

--- site/profile/manifests/ots.pp
+++ site/profile/manifests/ots.pp
@@ -25,39 +25,39 @@ class profile::ots {
 # gem install bundler:2.4.12
 # bundle update --bundler
 # Reboot needed after installation perhaps a few times
-  # rvm_system_ruby {
-  #   'ruby-3.1.4':
-  #     ensure      => 'present',
-  #     default_use => true,
-  #     # build_opts  => ['--binary'],
-  #     ;
-  #   # 'ruby-2.5.9':
-  #   #   ensure      => 'present',
-  #   #   default_use => false;
-  # }
-  # rvm_gemset {
-  #   'ruby-3.1.4@testing':
-  #     ensure  => present,
-  #     require => Rvm_system_ruby['ruby-3.1.4'];
-  # }
-  # rvm_gem {
-  #   'bundler':
-  #     ensure       => latest,
-  #     name         => 'bundler',
-  #     ruby_version => 'ruby-3.1.4',
-  #     require      => Rvm_system_ruby['ruby-3.1.4'];
-  # }
+  rvm_system_ruby {
+    'ruby-3.1.4':
+      ensure      => 'present',
+      default_use => true,
+      # build_opts  => ['--binary'],
+      ;
+    # 'ruby-2.5.9':
+    #   ensure      => 'present',
+    #   default_use => false;
+  }
+  rvm_gemset {
+    'ruby-3.1.4@testing':
+      ensure  => present,
+      require => Rvm_system_ruby['ruby-3.1.4'];
+  }
+  rvm_gem {
+    'bundler':
+      ensure       => latest,
+      name         => 'bundler',
+      ruby_version => 'ruby-3.1.4',
+      require      => Rvm_system_ruby['ruby-3.1.4'];
+  }
   # vcsrepo { "/opt/onetimesecret":
   #   ensure   => present,
   #   provider => git,
   #   source   => 'https://github.com/onetimesecret/onetimesecret.git',
   #   user     => 'root',
   # }
-  class { 'onetimesecret':
-    version        => 'v0.9.2',
-    secret         => 'SomeHardToGuessRandomCharacters',
-    redis_password => 'AnotherGoodPassword',
-  }
+  # class { 'onetimesecret':
+  #   version        => 'v0.16.1',
+  #   secret         => 'SomeHardToGuessRandomCharacters',
+  #   redis_password => 'AnotherGoodPassword',
+  # }
   # class { 'onetimesecret':
   #   version        => '945bb2a3cb43213f71e67abd0fa561267699d0eb',  #'e858f1edde6cc6af7ef75aa45f2bb9f9b0f0ecf5', #  e1156b1f8ab98322a898ee4defd1c3f0adb9b5d3
   #   install_dir    => '/opt',
