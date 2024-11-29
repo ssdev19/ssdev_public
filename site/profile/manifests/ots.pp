b43df7c3d158b118317c042ea90d52516277ab5d
anonss <belldex.inc@gmail.com>
2024-11-29 11:20:36 -0700

    s

--- site/profile/manifests/ots.pp
+++ site/profile/manifests/ots.pp
@@ -25,16 +25,16 @@ class profile::ots {
 # gem install bundler:2.4.12
 # bundle update --bundler
 # Reboot needed after installation perhaps a few times
-  # rvm_system_ruby {
-  #   'ruby-3.2.5':
-  #     ensure      => 'present',
-  #     default_use => true,
-  #     # build_opts  => ['--binary'],
-  #     ;
-  #   # 'ruby-2.5.9':
-  #   #   ensure      => 'present',
-  #   #   default_use => false;
-  # }
+  rvm_system_ruby {
+    'ruby-3.1.4':
+      ensure      => 'present',
+      default_use => true,
+      # build_opts  => ['--binary'],
+      ;
+    # 'ruby-2.5.9':
+    #   ensure      => 'present',
+    #   default_use => false;
+  }
   # rvm_gemset {
   #   'ruby-3.2.5@testing':
   #     ensure  => present,
