49b2d21efd835afe7767fd62a2e61343c5253d5f
anonss <belldex.inc@gmail.com>
2025-03-03 13:56:16 -0700

    d

--- site/profile/manifests/sso.pp
+++ site/profile/manifests/sso.pp
@@ -150,8 +150,8 @@ $line,
   file { "/opt/pingfederate-${pf_version}/pingfederate/server/default/deploy/pf-duo-security-adapter-3.1.2.jar":
     ensure => present,
     source => '/tmp/pingfederate/server/default/deploy/pf-duo-security-adapter-3.1.2.jar',
-    file_mode => '0775',
-    dir_mode  => '0775',
+    mode => '0775',
+    # dir_mode  => '0775',
     owner     => $pf_user,
     group     => $pf_user,
   }
