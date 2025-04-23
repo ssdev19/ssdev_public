abd6b30741208fa381a12913636a5e4bde8b1e00
anonss <belldex.inc@gmail.com>
2025-04-23 13:28:15 -0700

    d

--- site/profile/manifests/yourls2.pp
+++ site/profile/manifests/yourls2.pp
@@ -14,8 +14,24 @@ class profile::yourls2 (
   String $nginx_version,
 ) {
   include mysql::server
-  include nginx
+
   Package {['openldap-devel', 'make', 'yum-utils', 'pcre-devel', 'epel-release']:
     ensure => installed,
+  }
+  unless $::nginx_source {
+    archive { "/usr/src/nginx-${nginx_version}.tar.gz":
+      ensure       => present,
+      source       => "http://nginx.org/download/nginx-${nginx_version}.tar.gz",
+      extract_path => '/usr/src',
+      extract      => true,
+      provider     => 'wget',
+      cleanup      => false,
+    }
+    vcsrepo { "/usr/src/nginx-${nginx_version}/nginx-auth-ldap":
+      ensure   => present,
+      provider => git,
+      source   => 'https://github.com/kvspb/nginx-auth-ldap.git',
+      user     => 'root',
+    }
   }
 }
